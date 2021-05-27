a99a6b58afe4347cde3141472e30068071604a8a
Nils Werner <nils.werner@sonarsource.com>
2021-05-27 10:21:08 +0200

    SONARIAC-3 SonarIaC should highlight Terraform files

---
+++ iac-terraform-plugin/src/main/java/org/sonar/plugins/iac/terraform/visitors/SyntaxHighlightingVisitor.java
@@ -0,0 +1,62 @@
+/*
+ * SonarQube IaC Terraform Plugin
+ * Copyright (C) 2021-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.iac.terraform.visitors;
+
+import org.sonar.api.batch.sensor.highlighting.NewHighlighting;
+import org.sonar.api.batch.sensor.highlighting.TypeOfText;
+import org.sonar.plugins.iac.terraform.api.tree.BlockTree;
+import org.sonar.plugins.iac.terraform.api.tree.HasTextRange;
+import org.sonar.plugins.iac.terraform.api.tree.LabelTree;
+import org.sonar.plugins.iac.terraform.api.tree.LiteralExprTree;
+import org.sonar.plugins.iac.terraform.api.tree.Tree;
+import org.sonar.plugins.iac.terraform.api.tree.lexical.SyntaxToken;
+import org.sonar.plugins.iac.terraform.plugin.InputFileContext;
+
+import static org.sonar.api.batch.sensor.highlighting.TypeOfText.COMMENT;
+import static org.sonar.api.batch.sensor.highlighting.TypeOfText.CONSTANT;
+import static org.sonar.api.batch.sensor.highlighting.TypeOfText.KEYWORD;
+import static org.sonar.api.batch.sensor.highlighting.TypeOfText.STRING;
+
+public class SyntaxHighlightingVisitor extends TreeVisitor<InputFileContext> {
+
+  private NewHighlighting newHighlighting;
+
+  public SyntaxHighlightingVisitor() {
+    register(BlockTree.class,  (ctx, tree) -> highlight(ctx, tree.type(), KEYWORD));
+    register(LabelTree.class, (ctx, tree) -> highlight(ctx, tree, STRING));
+    register(LiteralExprTree.class, (ctx, tree) -> highlight(ctx, tree,  tree.is(Tree.Kind.STRING_LITERAL) ? STRING : CONSTANT));
+    register(SyntaxToken.class, (ctx, tree) -> tree.trivias().forEach(comment -> highlight(ctx, comment, COMMENT)));
+  }
+
+  @Override
+  protected void before(InputFileContext ctx, Tree root) {
+    newHighlighting = ctx.sensorContext.newHighlighting()
+      .onFile(ctx.inputFile);
+  }
+
+  @Override
+  protected void after(InputFileContext ctx, Tree root) {
+    newHighlighting.save();
+  }
+
+  private void highlight(InputFileContext ctx, HasTextRange range, TypeOfText typeOfText) {
+    newHighlighting.highlight(ctx.textRange(range.textRange()), typeOfText);
+  }
+}
