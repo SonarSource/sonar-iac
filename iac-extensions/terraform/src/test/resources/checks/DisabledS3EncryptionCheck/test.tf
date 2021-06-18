3a1c5f3362aa8f29b56fb8be74e0a1b6ab014709
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-06-18 12:34:26 +0200

    Add some tests for coverage

--- iac-extensions/terraform/src/test/resources/checks/DisabledS3EncryptionCheck/test.tf
+++ iac-extensions/terraform/src/test/resources/checks/DisabledS3EncryptionCheck/test.tf
@@ -5,6 +5,13 @@ resource "aws_s3_bucket" "mynoncompliantbuckets6245" { # Noncompliant
 resource "aws_s3_bucket" "mynoncompliantbuckets6245" { # Noncompliant
 }
 
+resource "aws_s3_bucket" "mynoncompliantbuckets6245" { # Noncompliant
+  bucket = "mycompliantbuckets6245"
+
+  not_server_side_encryption {
+  }
+}
+
 resource "aws_s3_bucket" "mycompliantbuckets6245" { # Compliant
   bucket = "mycompliantbuckets6245"
 
@@ -16,3 +23,7 @@ resource "aws_s3_bucket" "mycompliantbuckets6245" { # Compliant
     }
   }
 }
+
+resource "not_a_bucket" "mynoncompliantbuckets6245" {}
+
+resource {} # no labels
