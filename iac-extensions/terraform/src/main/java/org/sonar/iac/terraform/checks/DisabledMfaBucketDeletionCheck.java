abcc31c1c1377e07a5c377bb4f1b19624271c7f0
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-06-25 16:07:40 +0200

    Implement rule.

---
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/DisabledMfaBucketDeletionCheck.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+package org.sonar.iac.terraform.checks;
+
+import java.util.Optional;
+import org.sonar.check.Rule;
+import org.sonar.iac.common.api.checks.CheckContext;
+import org.sonar.iac.common.api.checks.SecondaryLocation;
+import org.sonar.iac.terraform.api.tree.AttributeTree;
+import org.sonar.iac.terraform.api.tree.BlockTree;
+import org.sonar.iac.terraform.checks.utils.AttributeUtils;
+import org.sonar.iac.terraform.checks.utils.StatementUtils;
+
+@Rule(key = "S6255")
+public class DisabledMfaBucketDeletionCheck extends AbstractResourceCheck {
+  private static final String MESSAGE = "Make sure allowing object deletion of a S3 versioned bucket without MFA is safe here.";
+  private static final String MESSAGE_SECONDARY = "Should be true";
+
+  @Override
+  protected void checkResource(CheckContext ctx, BlockTree tree) {
+    if (!isS3Bucket(tree)) {
+      return;
+    }
+
+    Optional<BlockTree> versioning = StatementUtils.getBlock(tree, "versioning");
+    if (versioning.isPresent()) {
+      Optional<AttributeTree> mfaDeleteAttribute = StatementUtils.getAttribute(versioning.get(), "mfa_delete");
+      if (!mfaDeleteAttribute.isPresent()) {
+        ctx.reportIssue(tree.labels().get(0), MESSAGE);
+      } else if (AttributeUtils.isFalse(mfaDeleteAttribute.get())) {
+        ctx.reportIssue(tree.labels().get(0), MESSAGE, new SecondaryLocation(mfaDeleteAttribute.get(), MESSAGE_SECONDARY));
+      }
+    } else {
+      ctx.reportIssue(tree.labels().get(0), MESSAGE);
+    }
+  }
+}
---
+++ iac-extensions/terraform/src/test/resources/checks/DisabledMfaBucketDeletionCheck/test.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "no_versioning" { # Noncompliant
+  bucket = "mycompliantbuckets6255"
+}
+
+resource "aws_s3_bucket" "mynoncompliantbuckets6255" { # Noncompliant
+  bucket = "mynoncompliantbuckets6255"
+
+  versioning {
+  }
+}
+
+resource "aws_s3_bucket" "mycompliantbuckets6255" { # Noncompliant
+  #      ^^^^^^^^^^^^^^^
+  bucket = "mycompliantbuckets6255"
+
+  versioning {
+    mfa_delete = false
+  }
+}
+
+resource "aws_s3_bucket" "mycompliantbuckets6255" { # Compliant
+  bucket = "mycompliantbuckets6255"
+
+  versioning {
+    enabled = true
+    mfa_delete = true
+  }
+}
