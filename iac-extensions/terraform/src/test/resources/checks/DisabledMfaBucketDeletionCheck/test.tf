8d0088f84553d836a5aa477ae8c6f292ed14048d
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-06-25 16:16:15 +0200

    Test issue message and secondary location

--- iac-extensions/terraform/src/test/resources/checks/DisabledMfaBucketDeletionCheck/test.tf
+++ iac-extensions/terraform/src/test/resources/checks/DisabledMfaBucketDeletionCheck/test.tf
@@ -9,12 +9,13 @@ resource "aws_s3_bucket" "mynoncompliantbuckets6255" { # Noncompliant
   }
 }
 
-resource "aws_s3_bucket" "mycompliantbuckets6255" { # Noncompliant
+resource "aws_s3_bucket" "mycompliantbuckets6255" { # Noncompliant {{Make sure allowing object deletion of a S3 versioned bucket without MFA is safe here.}}
   #      ^^^^^^^^^^^^^^^
   bucket = "mycompliantbuckets6255"
 
   versioning {
     mfa_delete = false
+  # ^^^^^^^^^^^^^^^^^^< {{Should be true}}
   }
 }
 
