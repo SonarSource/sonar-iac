0108985d4aad05acf64105065c1cb228c6294c32
Nils Werner <nils.werner@sonarsource.com>
2021-06-29 15:23:10 +0200

    License header...

--- iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/checks/utils/AttributeUtilsTest.java
+++ iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/checks/utils/AttributeUtilsTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2021-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
 package org.sonar.iac.terraform.checks.utils;
 
 import org.junit.jupiter.api.Test;
