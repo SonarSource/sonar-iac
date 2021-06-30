a25c9cdb56a145e6f07b33ea2efbbf7850d7dc41
Nils Werner <nils.werner@sonarsource.com>
2021-06-30 10:49:40 +0200

    Address review suggestions.

--- iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/checks/utils/LiteralUtilsTest.java
+++ iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/checks/utils/LiteralUtilsTest.java
@@ -7,12 +7,9 @@ package org.sonar.iac.terraform.checks.utils;
 
 
 import org.junit.jupiter.api.Test;
-import org.sonar.iac.terraform.api.tree.AttributeTree;
-import org.sonar.iac.terraform.api.tree.ExpressionTree;
 import org.sonar.iac.terraform.tree.impl.VariableExprTreeImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.sonar.iac.terraform.TestTreeBuilders.AttributeBuilder.attribute;
 import static org.sonar.iac.terraform.TestTreeBuilders.LiteralExprBuilder.booleanExpr;
 import static org.sonar.iac.terraform.TestTreeBuilders.LiteralExprBuilder.stringExpr;
 
@@ -20,15 +17,11 @@ class LiteralUtilsTest {
 
   @Test
   void test_isFalse() {
-    assertThat(AttributeUtils.isFalse(attr(booleanExpr("FALSE")))).isTrue();
-    assertThat(AttributeUtils.isFalse(attr(booleanExpr("TRUE")))).isFalse();
-    assertThat(AttributeUtils.isFalse(attr(booleanExpr("false")))).isTrue();
-    assertThat(AttributeUtils.isFalse(attr(stringExpr("false")))).isFalse();
-    assertThat(AttributeUtils.isFalse(attr(new VariableExprTreeImpl(null)))).isFalse();
-  }
-
-  private static AttributeTree attr(ExpressionTree value) {
-    return attribute().value(value).build();
+    assertThat(LiteralUtils.isFalse(booleanExpr("FALSE"))).isTrue();
+    assertThat(LiteralUtils.isFalse(booleanExpr("TRUE"))).isFalse();
+    assertThat(LiteralUtils.isFalse(booleanExpr("false"))).isTrue();
+    assertThat(LiteralUtils.isFalse(stringExpr("false"))).isFalse();
+    assertThat(LiteralUtils.isFalse(new VariableExprTreeImpl(null))).isFalse();
   }
 
 }
