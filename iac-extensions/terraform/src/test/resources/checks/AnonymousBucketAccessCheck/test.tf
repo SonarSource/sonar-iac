469913bc4033053671aec0c5e19c8b17da9ca6d4
Nils Werner <nils.werner@sonarsource.com>
2021-07-01 14:08:35 +0200

    SONARIAC-119 [S6270][TF] Having policies granting anonymous access to S3 buckets is security-sensitive

---
+++ iac-extensions/terraform/src/test/resources/checks/AnonymousBucketAccessCheck/test.tf
@@ -0,0 +1,115 @@
+resource "aws_s3_bucket_policy" "sensitive_policy_with_anonymous_access_literal" {  # Noncompliant {{Make sure this S3 policy granting anonymous access is safe here.}}
+#        ^^^^^^^^^^^^^^^^^^^^^^
+  bucket = aws_s3_bucket
+  policy = jsonencode({
+    Statement = [
+      {
+        Effect: "Allow",
+        Principal: "*",
+         #         ^^^< {{Anonymous access.}}
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_policy" "sensitive_policy_with_anonymous_access_object_mapping" {  # Noncompliant
+#        ^^^^^^^^^^^^^^^^^^^^^^
+  bucket = aws_s3_bucket
+  policy = jsonencode({
+    Statement = [
+      {
+        Effect: "Allow",
+        Principal: {
+          "AWS": "*"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_policy" "compliant_policy_with_denying_effect" {
+  bucket = aws_s3_bucket
+  policy = jsonencode({
+    Statement = [
+      {
+        Effect: "Deny",
+        Principal: "*",
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_policy" "compliant_policy_without_aws_principal" {
+  bucket = aws_s3_bucket
+  policy = jsonencode({
+    Statement = [{
+      Effect: "Allow",
+      Principal: {
+        "Foo": "*"
+      }
+    }]
+  })
+}
+
+resource "aws_s3_bucket" "sensitive_bucket_with_anonymous_access_literal" { # Noncompliant
+  policy = jsonencode({
+    Statement = [{
+        Effect    = "Allow"
+        Principal = "*"
+    }]
+  })
+}
+
+resource "aws_s3_bucket" "sensitive_bucket_with_anonymous_access_object_mapping" { # Noncompliant
+  policy = jsonencode({
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        "AWS": "*"
+      }
+    }]
+  })
+}
+
+resource "aws_s3_bucket" "compliant_bucket_with_denying_effect" {
+  policy = jsonencode({
+    Statement = [{
+      Effect    = "Deny"
+      Principal = "*"
+    }]
+  })
+}
+
+resource "aws_s3_bucket" "sensitive_policy_with_object_mapping_principal" { # Noncompliant
+#        ^^^^^^^^^^^^^^^
+  policy = jsonencode({
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        "AWS": [
+          "*"
+#         ^^^<
+        ]
+      }
+    }]
+  })
+}
+
+resource "not_a_bucket" "compliant_resource" { # Compliant
+}
+
+resource "aws_s3_bucket" "compliant_bucket_without_principal" { # Compliant
+  policy = jsonencode({
+    Statement = [{
+      Effect    = "Deny"
+    }]
+  })
+}
+
+resource "aws_s3_bucket" "compliant_bucket_without_effect" { # Compliant
+  policy = jsonencode({
+    Statement = [{
+      Principal = "*"
+    }]
+  })
+}
