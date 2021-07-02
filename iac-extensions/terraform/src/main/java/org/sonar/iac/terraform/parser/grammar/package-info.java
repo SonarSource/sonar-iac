7d50c5706b3f511f08001ac5eea4abc187269fd3
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2021-07-02 16:43:03 +0200

    Add missing package-info files (#122)

---
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/parser/grammar/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2021-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+@javax.annotation.ParametersAreNonnullByDefault
+package org.sonar.iac.terraform.parser.grammar;
