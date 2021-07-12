02e2f9158c0f972da530268a598e1cc244d80b24
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-07-12 13:47:04 +0200

    Improve S6265 locations CloudFormation

--- iac-extensions/cloudformation/src/test/java/org/sonar/iac/cloudformation/checks/BucketsAccessCheckTest.java
+++ iac-extensions/cloudformation/src/test/java/org/sonar/iac/cloudformation/checks/BucketsAccessCheckTest.java
@@ -6,6 +6,7 @@
 package org.sonar.iac.cloudformation.checks;
 
 import org.junit.jupiter.api.Test;
+import org.sonar.iac.common.api.checks.SecondaryLocation;
 import org.sonar.iac.common.testing.Verifier;
 
 import static org.sonar.iac.common.api.tree.impl.TextRanges.range;
@@ -20,8 +21,9 @@ class BucketsAccessCheckTest {
   @Test
   void test_json() {
     CloudformationVerifier.verify("BucketsAccessCheck/test.json", new BucketsAccessCheck(),
-      new Verifier.Issue(range(5, 14, 5, 31), "Make sure granting access to AllUsers group is safe here."),
-      new Verifier.Issue(range(12, 14, 12, 31), "Make sure granting access to AllUsers group is safe here."),
-      new Verifier.Issue(range(19, 14, 19, 31), "Make sure granting access to AuthenticatedUsers group is safe here."));
+      new Verifier.Issue(range(8, 25, 8, 42), "Make sure granting access to AllUsers group is safe here.",
+        new SecondaryLocation(range(5, 14, 5, 31), "Related bucket")),
+      new Verifier.Issue(range(15, 25, 15, 37), "Make sure granting access to AllUsers group is safe here."),
+      new Verifier.Issue(range(22, 25, 22, 44), "Make sure granting access to AuthenticatedUsers group is safe here."));
   }
 }
