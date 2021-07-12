9445d5da063c569c9ffead6b997812d8ba119eb1
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-07-12 17:21:41 +0200

    Improve S6281 locations CloudFormation

--- iac-extensions/cloudformation/src/main/java/org/sonar/iac/cloudformation/checks/BucketsPublicAclOrPolicyCheck.java
+++ iac-extensions/cloudformation/src/main/java/org/sonar/iac/cloudformation/checks/BucketsPublicAclOrPolicyCheck.java
@@ -6,9 +6,8 @@
 package org.sonar.iac.cloudformation.checks;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import org.sonar.check.Rule;
 import org.sonar.iac.cloudformation.api.tree.CloudformationTree;
@@ -23,15 +22,13 @@ import org.sonar.iac.common.checks.TextUtils;
 @Rule(key = "S6281")
 public class BucketsPublicAclOrPolicyCheck extends AbstractResourceCheck {
   private static final String MESSAGE = "Make sure allowing public policy/acl access is safe here.";
-  private static final String MISSING_MULTI_MSG = "\"%s\" settings are missing.";
-  private static final String MISSING_SINGLE_MSG = "\"%s\" setting is missing.";
-  private static final Map<String, String> ATTRIBUTE_TO_MESSAGE = new HashMap<>();
-  static {
-    ATTRIBUTE_TO_MESSAGE.put("BlockPublicAcls", "Public ACLs are allowed.");
-    ATTRIBUTE_TO_MESSAGE.put("BlockPublicPolicy", "Public Policies are allowed.");
-    ATTRIBUTE_TO_MESSAGE.put("IgnorePublicAcls", "Public ACLs are not ignored.");
-    ATTRIBUTE_TO_MESSAGE.put("RestrictPublicBuckets", "Public Buckets are not restricted.");
-  }
+  private static final String SECONDARY_MSG_PROPERTY = "set this property to true";
+  private static final String SECONDARY_MSG_BUCKET = "Related bucket";
+  private static final List<String> ATTRIBUTES_TO_CHECK = Arrays.asList(
+    "BlockPublicAcls",
+    "BlockPublicPolicy",
+    "IgnorePublicAcls",
+    "RestrictPublicBuckets");
 
   @Override
   protected void checkResource(CheckContext ctx, Resource resource) {
@@ -48,37 +45,25 @@ public class BucketsPublicAclOrPolicyCheck extends AbstractResourceCheck {
   }
 
   private static void checkConfiguration(CheckContext ctx, Resource resource, CloudformationTree configuration) {
-    List<SecondaryLocation> problems = configurationProblems(configuration);
-    if (!problems.isEmpty()) {
-      ctx.reportIssue(resource.type(), MESSAGE, problems);
-    }
+    List<SecondaryLocation> problemsAsSecondaryLocations = configurationProblemsAsSecondaryLocations(configuration);
+    CloudformationTree primaryLocationTree = getKey(resource.properties()).orElse(configuration);
 
-    List<String> missingSettings = missingSettings(configuration);
-    if (configuration instanceof MappingTree && !missingSettings.isEmpty()) {
-      String secondaryMessage = String.format(MISSING_SINGLE_MSG, missingSettings.get(0));
-      if (missingSettings.size() > 1) {
-        secondaryMessage = String.format(MISSING_MULTI_MSG, String.join(", ", missingSettings));
-      }
-      ctx.reportIssue(resource.type(), MESSAGE, new SecondaryLocation(getKey(resource.properties()).orElse(configuration), secondaryMessage));
+    if (!problemsAsSecondaryLocations.isEmpty() || hasMissingSetting(configuration)) {
+      problemsAsSecondaryLocations.add(new SecondaryLocation(resource.type(), SECONDARY_MSG_BUCKET));
+      ctx.reportIssue(primaryLocationTree, MESSAGE, problemsAsSecondaryLocations);
     }
   }
 
-  private static List<SecondaryLocation> configurationProblems(CloudformationTree configuration) {
+  private static boolean hasMissingSetting(CloudformationTree configuration) {
+    return ATTRIBUTES_TO_CHECK.stream().anyMatch(a -> MappingTreeUtils.hasValue(configuration, a).isFalse());
+  }
+
+  private static List<SecondaryLocation> configurationProblemsAsSecondaryLocations(CloudformationTree configuration) {
     List<SecondaryLocation> problems = new ArrayList<>();
-    ATTRIBUTE_TO_MESSAGE.forEach((attribute, message) -> MappingTreeUtils.getValue(configuration, attribute)
+    ATTRIBUTES_TO_CHECK.forEach(attribute -> MappingTreeUtils.getValue(configuration, attribute)
       .filter(TextUtils::isValueFalse)
-      .ifPresent(c -> problems.add(new SecondaryLocation(c, message))));
+      .ifPresent(c -> problems.add(new SecondaryLocation(c, SECONDARY_MSG_PROPERTY))));
     return problems;
-  }
-
-  private static List<String> missingSettings(CloudformationTree configuration) {
-    List<String> result = new ArrayList<>();
-    for (String attribute : ATTRIBUTE_TO_MESSAGE.keySet()) {
-      if (!MappingTreeUtils.getValue(configuration, attribute).isPresent()) {
-        result.add(attribute);
-      }
-    }
-    return result;
   }
 
   private static Optional<CloudformationTree> getKey(CloudformationTree properties) {
