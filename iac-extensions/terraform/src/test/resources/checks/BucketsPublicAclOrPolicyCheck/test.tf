2b2b08b53e4b68f04ce8f9172f3d7a0afc24cc5d
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-07-12 17:54:43 +0200

    Improve S6281 locations Terraform

--- iac-extensions/terraform/src/test/resources/checks/BucketsPublicAclOrPolicyCheck/test.tf
+++ iac-extensions/terraform/src/test/resources/checks/BucketsPublicAclOrPolicyCheck/test.tf
@@ -11,28 +11,31 @@ resource "aws_s3_bucket_public_access_block" "mycompliants6281_publicaccess" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket" "mynoncompliantfirstbuckets6281default" { # Noncompliant
+resource "aws_s3_bucket" "mynoncompliantfirstbuckets6281default" { # Noncompliant {{Make sure allowing public policy/acl access is safe here.}}
   bucket = "mynoncompliantfirstbuckets6281defaultname"
 }
 
-resource "aws_s3_bucket" "mynoncompliantfirstbuckets6281" {  # Noncompliant
+resource "aws_s3_bucket" "mynoncompliantfirstbuckets6281" {
+  #      ^^^^^^^^^^^^^^^> {{Related bucket}}
   bucket = "mynoncompliantfirstbuckets6281name"
 }
 
-resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_1" {
+resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_1" { # Noncompliant {{Make sure allowing public policy/acl access is safe here.}}
+  #      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   bucket = aws_s3_bucket.mynoncompliantfirstbuckets6281.id
 
-  block_public_acls       = false # secondary location
+  block_public_acls       = false
+  #                         ^^^^^< {{set this property to true}}
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket" "mynoncompliantsecondbuckets6281" {  # Noncompliant
+resource "aws_s3_bucket" "mynoncompliantsecondbuckets6281" {
   bucket = "mynoncompliantsecondbuckets6281name"
 }
 
-resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_2" {
+resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_2" { # Noncompliant
   bucket = aws_s3_bucket.mynoncompliantsecondbuckets6281.id
 
   block_public_acls       = true
@@ -41,11 +44,11 @@ resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_2
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket" "mynoncompliantthirdbuckets6281" {  # Noncompliant
+resource "aws_s3_bucket" "mynoncompliantthirdbuckets6281" {
   bucket = "mynoncompliantthirdbuckets6281name"
 }
 
-resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_3" {
+resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_3" { # Noncompliant
   bucket = aws_s3_bucket.mynoncompliantthirdbuckets6281.id
 
   block_public_acls       = true
@@ -54,11 +57,11 @@ resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_3
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket" "mynoncompliantfourthbuckets6281" {  # Noncompliant
+resource "aws_s3_bucket" "mynoncompliantfourthbuckets6281" {
   bucket = "mynoncompliantfourthbuckets6281name"
 }
 
-resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_4" {
+resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_4" { # Noncompliant
   bucket = "mynoncompliantfourthbuckets6281name"
 
   block_public_acls       = true
@@ -67,13 +70,13 @@ resource "aws_s3_bucket_public_access_block" "mynoncompliants6281_publicaccess_4
   restrict_public_buckets = false # secondary location
 }
 
-resource "aws_s3_bucket" "mynoncompliantmissingbuckets6234" {  # Noncompliant
-  #      ^^^^^^^^^^^^^^^
+resource "aws_s3_bucket" "mynoncompliantmissingbuckets6234" {
+  #      ^^^^^^^^^^^^^^^> {{Related bucket}}
   bucket = "mynoncompliantmissingbuckets6234name"
 }
 
-resource "aws_s3_bucket_public_access_block" "mynoncompliants6234_missing" {
-  #      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^< {{"restrict_public_buckets" setting is missing.}}
+resource "aws_s3_bucket_public_access_block" "mynoncompliants6234_missing" { # Noncompliant {{Make sure allowing public policy/acl access is safe here.}}
+  #      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   bucket = "mynoncompliantmissingbuckets6234name"
 
   block_public_acls       = true
@@ -81,13 +84,13 @@ resource "aws_s3_bucket_public_access_block" "mynoncompliants6234_missing" {
   ignore_public_acls      = true
 }
 
-resource "aws_s3_bucket" "mynoncompliantmultimissingbuckets61234" {  # Noncompliant
-  #      ^^^^^^^^^^^^^^^
+resource "aws_s3_bucket" "mynoncompliantmultimissingbuckets61234" {
+  #      ^^^^^^^^^^^^^^^> {{Related bucket}}
   bucket = "mynoncompliantmultimissingbuckets61234name"
 }
 
-resource "aws_s3_bucket_public_access_block" "mynoncompliants6231_multiple_missing" {
-  #      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^< {{"ignore_public_acls", "restrict_public_buckets" settings are missing.}}
+resource "aws_s3_bucket_public_access_block" "mynoncompliants6231_multiple_missing" { # Noncompliant {{Make sure allowing public policy/acl access is safe here.}}
+  #      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   bucket = "mynoncompliantmultimissingbuckets61234name"
 
   block_public_acls       = true
