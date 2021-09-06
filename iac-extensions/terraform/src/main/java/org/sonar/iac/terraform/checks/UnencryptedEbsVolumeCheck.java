6a73bb10ac04743716de5d460f3985ca8ec1b0e5
Michael Gumowski <michael.gumowski@sonarsource.com>
2021-09-06 15:36:51 +0200

    SONARIAC-126 Implements terraform:S6275

---
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/UnencryptedEbsVolumeCheck.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2021-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+package org.sonar.iac.terraform.checks;
+
+import java.util.List;
+import java.util.Optional;
+import org.sonar.check.Rule;
+import org.sonar.iac.common.api.checks.CheckContext;
+import org.sonar.iac.common.checks.PropertyUtils;
+import org.sonar.iac.common.checks.TextUtils;
+import org.sonar.iac.terraform.api.tree.BlockTree;
+import org.sonar.iac.terraform.api.tree.LabelTree;
+import org.sonar.iac.terraform.api.tree.LiteralExprTree;
+import org.sonar.iac.terraform.api.tree.TerraformTree;
+
+@Rule(key = "S6275")
+public class UnencryptedEbsVolumeCheck extends AbstractResourceCheck {
+
+  private static final String[] PROPERTIES = new String[] {"root_block_device", "ebs_block_device"};
+  private static final String MESSAGE = "Make sure that using unencrypted volumes is safe here.";
+
+  @Override
+  protected void checkResource(CheckContext ctx, BlockTree resource) {
+    List<LabelTree> labels = resource.labels();
+    if (labels.isEmpty()) {
+      return;
+    }
+    String resourceName = labels.get(0).value();
+    switch (resourceName) {
+      case "aws_ebs_encryption_by_default":
+        checkEncrypted(ctx, resource, "enabled", true);
+        break;
+      case "aws_ebs_volume":
+        checkEncrypted(ctx, resource, "encrypted", false);
+        break;
+      case "aws_launch_configuration":
+        checkEncryptionProperties(ctx, resource);
+        break;
+      default:
+        // do nothing
+    }
+  }
+
+  private static void checkEncryptionProperties(CheckContext ctx, BlockTree resource) {
+    for (String propertyName : PROPERTIES) {
+      PropertyUtils.get(resource, propertyName, BlockTree.class)
+        .ifPresent(propertyTree -> checkEncrypted(ctx, propertyTree, "encrypted", false));
+    }
+  }
+
+  private static void checkEncrypted(CheckContext ctx, BlockTree tree, String key, boolean secureByDefault) {
+    Optional<LiteralExprTree> booleanProperty = PropertyUtils.value(tree, key, TerraformTree.class)
+      .filter(x -> x.is(TerraformTree.Kind.BOOLEAN_LITERAL))
+      .map(LiteralExprTree.class::cast);
+
+    if (!booleanProperty.isPresent()) {
+      if (secureByDefault) {
+        return;
+      }
+      List<LabelTree> labels = tree.labels();
+      ctx.reportIssue(labels.isEmpty() ? tree.key() : labels.get(0), MESSAGE);
+      return;
+    }
+
+    booleanProperty
+      .filter(TextUtils::isValueFalse)
+      .ifPresent(value -> ctx.reportIssue(value, MESSAGE));
+  }
+}
---
+++ iac-extensions/terraform/src/test/resources/checks/UnencryptedEbsVolumeCheck/test.tf
@@ -0,0 +1,60 @@
+resource "aws_ebs_volume" "ebs_volume" { # Noncompliant {{Make sure that using unencrypted volumes is safe here.}}
+#        ^^^^^^^^^^^^^^^^
+}
+
+resource "aws_ebs_volume" "ebs_volume" {
+  encrypted = false # Noncompliant {{Make sure that using unencrypted volumes is safe here.}}
+#             ^^^^^
+}
+resource "aws_ebs_volume" "ebs_volume" {
+  encrypted = true
+}
+
+resource "aws_ebs_encryption_by_default" "default_encryption" {
+  enabled = false # Noncompliant
+#           ^^^^^
+}
+resource "aws_ebs_encryption_by_default" "default_encryption_1" { # Compliant
+}
+
+resource "aws_ebs_encryption_by_default" "default_encryption_2" { # Compliant
+  enabled = true
+}
+
+resource "aws_launch_configuration" "launch_configuration" {
+  root_block_device { # Noncompliant {{Make sure that using unencrypted volumes is safe here.}}
+# ^^^^^^^^^^^^^^^^^
+  }
+  ebs_block_device { # Noncompliant
+# ^^^^^^^^^^^^^^^^
+  }
+}
+
+resource "aws_launch_configuration" "launch_configuration" {
+  root_block_device {
+    encrypted = false # Noncompliant {{Make sure that using unencrypted volumes is safe here.}}
+#               ^^^^^
+  }
+  ebs_block_device {
+    encrypted = false # Noncompliant {{Make sure that using unencrypted volumes is safe here.}}
+#               ^^^^^
+  }
+}
+
+resource "aws_launch_configuration" "launch_configuration" {
+  root_block_device {
+    encrypted = true
+  }
+  ebs_block_device {
+    encrypted = true
+  }
+}
+
+resource "locals" {
+  enabled = false
+}
+
+resource {
+  # unnamed resource ?
+}
+
