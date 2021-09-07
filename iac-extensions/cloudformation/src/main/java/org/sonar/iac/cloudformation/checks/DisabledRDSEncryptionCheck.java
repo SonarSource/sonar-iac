cff19b4196473b6ccbf3192b981524d0b1614d57
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2021-09-07 10:41:50 +0200

    Fix after review

--- iac-extensions/cloudformation/src/main/java/org/sonar/iac/cloudformation/checks/DisabledRDSEncryptionCheck.java
+++ iac-extensions/cloudformation/src/main/java/org/sonar/iac/cloudformation/checks/DisabledRDSEncryptionCheck.java
@@ -5,8 +5,12 @@
  */
 package org.sonar.iac.cloudformation.checks;
 
+import java.util.Optional;
+
 import org.sonar.check.Rule;
 import org.sonar.iac.common.api.checks.CheckContext;
+import org.sonar.iac.common.api.checks.SecondaryLocation;
+import org.sonar.iac.common.api.tree.PropertyTree;
 import org.sonar.iac.common.checks.PropertyUtils;
 import org.sonar.iac.common.checks.TextUtils;
 
@@ -14,15 +18,22 @@ import org.sonar.iac.common.checks.TextUtils;
 public class DisabledRDSEncryptionCheck extends AbstractResourceCheck {
 
   private static final String MESSAGE = "Make sure that using unencrypted databases is safe here.";
+  private static final String SECONDARY_MESSAGE = "Related RDS DBInstance";
+  private static final String STORAGE_ENCRYPTED = "StorageEncrypted";
 
   @Override
   protected void checkResource(CheckContext ctx, Resource resource) {
-    if (resource.isType("AWS::RDS::DBInstance") && !isStorageEncrypted(resource)) {
+    if (!resource.isType("AWS::RDS::DBInstance")) {
+      return;
+    }
+    Optional<PropertyTree> maybeEncryption = PropertyUtils.get(resource.properties(), STORAGE_ENCRYPTED);
+    if (maybeEncryption.isPresent()) {
+      PropertyTree encryption = maybeEncryption.get();
+      if (TextUtils.isValueFalse(encryption.value())) {
+        ctx.reportIssue(encryption.key(), MESSAGE, new SecondaryLocation(resource.type(), SECONDARY_MESSAGE));
+      }
+    } else {
       ctx.reportIssue(resource.type(), MESSAGE);
     }
-  }
-
-  private static final boolean isStorageEncrypted(Resource resource) {
-    return PropertyUtils.value(resource.properties(), "StorageEncrypted").filter(TextUtils::isValueTrue).isPresent();
   }
 }
--- iac-extensions/cloudformation/src/test/java/org/sonar/iac/cloudformation/checks/DisabledRDSEncryptionCheckTest.java
+++ iac-extensions/cloudformation/src/test/java/org/sonar/iac/cloudformation/checks/DisabledRDSEncryptionCheckTest.java
@@ -6,6 +6,7 @@
 package org.sonar.iac.cloudformation.checks;
 
 import org.junit.jupiter.api.Test;
+import org.sonar.iac.common.api.checks.SecondaryLocation;
 import org.sonar.iac.common.testing.Verifier;
 
 import static org.sonar.iac.common.api.tree.impl.TextRanges.range;
@@ -19,9 +20,10 @@ class DisabledRDSEncryptionCheckTest {
 
   @Test
   void test_json() {
+    String message = "Make sure that using unencrypted databases is safe here.";
     CloudformationVerifier.verify("DisabledRDSEncryptionCheck/test.json", new DisabledRDSEncryptionCheck(),
-      new Verifier.Issue(range(11, 14, 11, 36), "Make sure that using unencrypted databases is safe here."),
-      new Verifier.Issue(range(17, 14, 17, 36)));
+      new Verifier.Issue(range(13, 8, 13, 26), message, new SecondaryLocation(range(11, 14, 11, 36), "Related RDS DBInstance")),
+      new Verifier.Issue(range(17, 14, 17, 36), message));
   }
 
 }
