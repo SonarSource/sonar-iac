79ab528ba01a89270b3e565c7aa16f321a51cb17
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2021-09-07 11:05:13 +0200

    Fix after review

--- iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/DisabledRDSEncryptionCheck.java
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/DisabledRDSEncryptionCheck.java
@@ -5,25 +5,36 @@
  */
 package org.sonar.iac.terraform.checks;
 
+import java.util.Optional;
+
 import org.sonar.check.Rule;
 import org.sonar.iac.common.api.checks.CheckContext;
+import org.sonar.iac.common.api.checks.SecondaryLocation;
+import org.sonar.iac.common.api.tree.PropertyTree;
 import org.sonar.iac.common.checks.PropertyUtils;
 import org.sonar.iac.common.checks.TextUtils;
 import org.sonar.iac.terraform.api.tree.BlockTree;
+
 
 @Rule(key = "S6303")
 public class DisabledRDSEncryptionCheck extends AbstractResourceCheck {
 
   private static final String MESSAGE = "Make sure that using unencrypted databases is safe here.";
+  private static final String SECONDARY_MESSAGE = "Related RDS DBInstance";
 
   @Override
   protected void checkResource(CheckContext ctx, BlockTree resource) {
-    if (isResource(resource, "aws_db_instance") && !isStorageEncrypted(resource)) {
+    if (!isResource(resource, "aws_db_instance")) {
+      return;
+    }
+    Optional<PropertyTree> maybeEncryption = PropertyUtils.get(resource, "storage_encrypted");
+    if (maybeEncryption.isPresent()) {
+      PropertyTree encryption = maybeEncryption.get();
+      if (TextUtils.isValueFalse(encryption.value())) {
+        ctx.reportIssue(encryption.key(), MESSAGE, new SecondaryLocation(resource.labels().get(0), SECONDARY_MESSAGE));
+      }
+    } else {
       ctx.reportIssue(resource.labels().get(0), MESSAGE);
     }
-  }
-
-  private static final boolean isStorageEncrypted(BlockTree resource) {
-    return PropertyUtils.value(resource, "storage_encrypted").filter(TextUtils::isValueTrue).isPresent();
   }
 }
--- iac-extensions/terraform/src/test/resources/checks/DisabledRDSEncryptionCheck/test.tf
+++ iac-extensions/terraform/src/test/resources/checks/DisabledRDSEncryptionCheck/test.tf
@@ -2,12 +2,14 @@ resource "aws_db_instance" "encrypted" {
   storage_encrypted = true
 }
 
-resource "aws_db_instance" "unencrypted1" { # Noncompliant
-  storage_encrypted = false
+resource "aws_db_instance" "unencrypted1" {
+#        ^^^^^^^^^^^^^^^^^>
+  storage_encrypted = false # Noncompliant {{Make sure that using unencrypted databases is safe here.}}
+# ^^^^^^^^^^^^^^^^^
 }
 
-resource "aws_db_instance" "unencrypted2" { # Noncompliant
-  
+resource "aws_db_instance" "unencrypted2" { # Noncompliant {{Make sure that using unencrypted databases is safe here.}}
+#        ^^^^^^^^^^^^^^^^^
 }
 
 resource "no_aws_db_instance" "whatever" {
