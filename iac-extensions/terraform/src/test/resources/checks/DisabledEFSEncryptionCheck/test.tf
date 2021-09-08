38084b4bc4ce4b67dd6c67f8a730009958f902fa
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2021-09-08 10:29:14 +0200

    SONARIAC-179 [TF] S6332 Using unencrypted EFS file systems is security-sensitive

---
+++ iac-extensions/terraform/src/test/resources/checks/DisabledEFSEncryptionCheck/test.tf
@@ -0,0 +1,16 @@
+resource "aws_efs_file_system" "fs" { # Noncompliant {{Make sure that using unencrypted EFS file systems is safe here.}}
+#        ^^^^^^^^^^^^^^^^^^^^^
+}
+
+resource "aws_efs_file_system" "fs" {
+#        ^^^^^^^^^^^^^^^^^^^^^> {{Related file system}}
+  encrypted = false  # Noncompliant {{Make sure that using unencrypted EFS file systems is safe here.}}
+# ^^^^^^^^^
+}
+
+resource "aws_efs_file_system" "fs" { # Compliant
+  encrypted = true  
+}
+
+resource "some_other_type" "other" {
+}
