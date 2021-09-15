2bc823a5b8316fd5622391628e450e67265aeb42
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-09-15 15:58:14 +0200

    SONARIAC-162 [CF] S6321 Administration services access should be restricted to specific IP addresses

---
+++ iac-extensions/cloudformation/src/test/java/org/sonar/iac/cloudformation/checks/UnrestrictedAdministrationCheckTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2021-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+package org.sonar.iac.cloudformation.checks;
+
+import org.junit.jupiter.api.Test;
+
+class UnrestrictedAdministrationCheckTest {
+
+  @Test
+  void test_yaml() {
+    CloudformationVerifier.verify("UnrestrictedAdministrationCheck/test.yaml", new UnrestrictedAdministrationCheck());
+  }
+}
