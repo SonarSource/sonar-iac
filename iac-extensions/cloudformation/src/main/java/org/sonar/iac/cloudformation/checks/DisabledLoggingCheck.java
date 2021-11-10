b4379994d42465088bd3abf5669e7f14fca24c9a
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-11-10 12:14:31 +0100

    Make method static

--- iac-extensions/cloudformation/src/main/java/org/sonar/iac/cloudformation/checks/DisabledLoggingCheck.java
+++ iac-extensions/cloudformation/src/main/java/org/sonar/iac/cloudformation/checks/DisabledLoggingCheck.java
@@ -42,7 +42,7 @@ public class DisabledLoggingCheck extends AbstractResourceCheck {
     }
   }
 
-  private void checkS3Bucket(CheckContext ctx, Resource resource) {
+  private static void checkS3Bucket(CheckContext ctx, Resource resource) {
     CloudformationTree properties = resource.properties();
     if (PropertyUtils.value(properties, "LoggingConfiguration").isEmpty() && !isMaybeLoggingBucket(properties)) {
       ctx.reportIssue(resource.type(), MESSAGE);
