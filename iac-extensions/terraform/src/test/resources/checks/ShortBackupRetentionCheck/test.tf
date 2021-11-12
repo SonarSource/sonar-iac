266a5190bd799feb49ba75aeb5749c53b9af7277
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2021-11-12 13:33:29 +0100

    [TF] SONARIAC-259 Rule S6364 : Reducing Backup retention duration is security-sensitive (AWS RDS) (#241)
    * [TF] SONARIAC-259 Rule S6364 : Reducing Backup retention duration is security-sensitive (AWS RDS)
    
    * Update ruling
    
    Co-authored-by: Karim El Ouerghemmi <64004037+karim-ouerghemmi-sonarsource@users.noreply.github.com>

---
+++ iac-extensions/terraform/src/test/resources/checks/ShortBackupRetentionCheck/test.tf
@@ -0,0 +1,49 @@
+resource "aws_db_instance" "too_low_period" {
+  name                 = "non_compliant_db"
+  backup_retention_period = 2 # Noncompliant {{Make sure that defining a short backup retention duration is safe here.}}
+                          # ^
+}
+
+resource "aws_db_instance" "missing_period" { # Noncompliant {{Make sure that defining a short backup retention duration is safe here.}}
+  #      ^^^^^^^^^^^^^^^^^
+  name                 = "non_compliant_db"
+}
+
+resource "aws_db_instance" "read_replica_db" {
+  name                 = "compliant_db"
+  source_db_instance_identifier = XXXX.arn
+}
+
+resource "aws_db_instance" "compliant_period" {
+  name                 = "compliant_db"
+  backup_retention_period = 7
+}
+
+resource "aws_db_instance" "compliant_var_period" {
+  name                 = "compliant_db"
+  backup_retention_period = var.period
+}
+
+resource "aws_rds_cluster" "too_low_period" {
+  name                 = "non_compliant_db"
+  backup_retention_period = 2 # Noncompliant
+  #                         ^
+}
+
+resource "aws_rds_cluster" "missing_period" { # Noncompliant
+  name                 = "non_compliant_db"
+}
+
+resource "aws_rds_cluster" "compliant_period" {
+  name                 = "compliant_db"
+  backup_retention_period = 7
+}
+
+resource "aws_rds_cluster" "compliant_var_period" {
+  name                 = "compliant_db"
+  backup_retention_period = var.period
+}
+
+
+resource "non_relevant_resource" "for_coverage" {
+}
