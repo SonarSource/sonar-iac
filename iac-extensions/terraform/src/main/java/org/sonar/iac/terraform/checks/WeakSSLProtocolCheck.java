1133b592911540e1f048cbe81ba83b1c5e1bb2e5
Nils Werner <nils.werner@sonarsource.com>
2021-12-13 14:52:46 +0100

    Make AbstractMultipleResourcesCheck::registerChecks protected

--- iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/WeakSSLProtocolCheck.java
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/WeakSSLProtocolCheck.java
@@ -35,7 +35,7 @@ public class WeakSSLProtocolCheck extends AbstractMultipleResourcesCheck {
   public static final String SECURITY_POLICY = "security_policy";
 
   @Override
-  void registerChecks() {
+  protected void registerChecks() {
     register(WeakSSLProtocolCheck::checkApiGatewayDomainName, "aws_api_gateway_domain_name");
     register(WeakSSLProtocolCheck::checkApiGatewayV2DomainName, "aws_apigatewayv2_domain_name");
     register(WeakSSLProtocolCheck::checkElasticsearchDomain, "aws_elasticsearch_domain");
