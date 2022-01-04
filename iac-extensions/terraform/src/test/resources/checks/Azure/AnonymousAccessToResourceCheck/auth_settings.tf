8e756c8e1f0027b57f23d13169677801d89044af
Nils Werner <nils.werner@sonarsource.com>
2022-01-04 11:08:42 +0100

    Add additional test case for azurerm_app_service_slot

--- iac-extensions/terraform/src/test/resources/checks/Azure/AnonymousAccessToResourceCheck/auth_settings.tf
+++ iac-extensions/terraform/src/test/resources/checks/Azure/AnonymousAccessToResourceCheck/auth_settings.tf
@@ -34,6 +34,14 @@ resource "azurerm_windows_web_app" "sensitive_web_app" { # Noncompliant
 }
 resource "azurerm_linux_web_app" "sensitive_web_app" { # Noncompliant
 }
+
+resource "azurerm_app_service_slot" "compliant_web_app" {
+  auth_settings {
+    enabled                       = true
+    unauthenticated_client_action = "RedirectToLoginPage"
+  }
+}
+
 resource "other_resource" "coverage" {
   auth_settings {
     enabled = false
