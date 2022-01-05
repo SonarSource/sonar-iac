f6c203db8e6ddd160a45d05494ee92a25c9552db
Nils Werner <nils.werner@sonarsource.com>
2022-01-05 10:41:01 +0100

    Adapt new test case to highlighting strategy

--- iac-extensions/terraform/src/test/resources/checks/Azure/RoleBasedAccessControlCheck/key_vault.tf
+++ iac-extensions/terraform/src/test/resources/checks/Azure/RoleBasedAccessControlCheck/key_vault.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault" "production" {
   # Noncompliant@+1 {{Make sure that disabling role-based access control is safe here.}}
   enable_rbac_authorization   = false
-#                               ^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
 
 # Noncompliant@+1 {{Omitting 'enable_rbac_authorization' disables role-based access control for this resource. Make sure it is safe here.}}
