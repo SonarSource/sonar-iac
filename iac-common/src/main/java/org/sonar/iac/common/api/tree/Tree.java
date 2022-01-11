98847def550ded1c9c12810806bae2fd3e60d932
Nils Werner <nils.werner@sonarsource.com>
2022-01-11 10:06:19 +0100

    Happy new year 2022!

--- iac-common/src/main/java/org/sonar/iac/common/api/tree/Tree.java
+++ iac-common/src/main/java/org/sonar/iac/common/api/tree/Tree.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube IaC Plugin
- * Copyright (C) 2021-2021 SonarSource SA
+ * Copyright (C) 2021-2022 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
