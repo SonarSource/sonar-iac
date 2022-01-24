8f1b2adfc431c072878fc47644432f6dd4055c99
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2022-01-24 08:51:32 +0100

    SONARIAC-280 Rule S6388: Using unencrypted cloud services is security-sensitive (#296)
    * SONARIAC-303
    * SONARIAC-304
    * SONARIAC-305
    * SONARIAC-306

---
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/UnencryptedCloudServicesCheck.java
@@ -0,0 +1,57 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.iac.terraform.checks;
+
+import org.sonar.check.Rule;
+
+import java.util.List;
+
+@Rule(key = "S6388")
+public class UnencryptedCloudServicesCheck extends ResourceVisitor {
+  public static final String UNENCRYPTED_MESSAGE = "Make sure using unencrypted cloud storage is safe here.";
+  public static final String FORMAT_OMITTING = "Omitting %s enables clear-text storage. Make sure it is safe here.";
+
+  @Override
+  protected void registerResourceConsumer() {
+    register("azurerm_data_lake_store",
+      resource -> resource.attribute("encryption_state")
+        .reportIfValueMatches("Disabled", UNENCRYPTED_MESSAGE));
+
+    register("azurerm_managed_disk",
+      resource -> resource.attribute("disk_encryption_set_id")
+        .reportIfAbsence(FORMAT_OMITTING));
+
+    register("azurerm_mysql_server",
+      resource -> resource.attribute("infrastructure_encryption_enabled")
+        .reportIfAbsence(FORMAT_OMITTING)
+        .reportIfFalse(UNENCRYPTED_MESSAGE));
+
+    register("azurerm_windows_virtual_machine_scale_set",
+      resource -> resource.attribute("encryption_at_host_enabled")
+        .reportIfAbsence(FORMAT_OMITTING)
+        .reportIfFalse(UNENCRYPTED_MESSAGE));
+
+    register("azurerm_windows_virtual_machine_scale_set",
+      resource -> List.of("os_disk", "data_disk")
+        .forEach(blockName -> resource.block(blockName)
+          .ifPresent(block -> block.attribute("disk_encryption_set_id")
+            .reportIfAbsence(FORMAT_OMITTING))));
+  }
+}
