c6511d2c50c9adef0d34cb68b97ddcf8d583006c
Nils Werner <nils.werner@sonarsource.com>
2022-02-10 14:14:13 +0100

    SONARIAC-370 Update Terraform syntax highlighting

--- iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/visitors/TerraformHighlightingVisitor.java
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/visitors/TerraformHighlightingVisitor.java
@@ -20,22 +20,38 @@
 package org.sonar.iac.terraform.visitors;
 
 import org.sonar.iac.common.extension.visitors.SyntaxHighlightingVisitor;
+import org.sonar.iac.terraform.api.tree.AttributeAccessTree;
+import org.sonar.iac.terraform.api.tree.AttributeTree;
 import org.sonar.iac.terraform.api.tree.BlockTree;
 import org.sonar.iac.terraform.api.tree.LabelTree;
 import org.sonar.iac.terraform.api.tree.LiteralExprTree;
+import org.sonar.iac.terraform.api.tree.ObjectElementTree;
 import org.sonar.iac.terraform.api.tree.TerraformTree;
 
+import static org.sonar.api.batch.sensor.highlighting.TypeOfText.ANNOTATION;
 import static org.sonar.api.batch.sensor.highlighting.TypeOfText.CONSTANT;
 import static org.sonar.api.batch.sensor.highlighting.TypeOfText.KEYWORD;
+import static org.sonar.api.batch.sensor.highlighting.TypeOfText.KEYWORD_LIGHT;
 import static org.sonar.api.batch.sensor.highlighting.TypeOfText.STRING;
 
 public class TerraformHighlightingVisitor extends SyntaxHighlightingVisitor {
 
   @Override
   protected void languageSpecificHighlighting() {
-    register(BlockTree.class,  (ctx, tree) -> highlight(tree.key(), KEYWORD));
-    register(LabelTree.class, (ctx, tree) -> highlight(tree, STRING));
-    register(LiteralExprTree.class, (ctx, tree) -> highlight(tree,  tree.is(TerraformTree.Kind.STRING_LITERAL) ? STRING : CONSTANT));
+    register(BlockTree.class,  (ctx, tree) -> highlight(tree.key(), CONSTANT));
+    register(LabelTree.class, (ctx, tree) -> highlight(tree, KEYWORD));
+    register(AttributeAccessTree.class, (ctx, tree) -> highlight(tree, KEYWORD));
+    register(LiteralExprTree.class, (ctx, tree) -> {
+      if (tree.is(TerraformTree.Kind.STRING_LITERAL, TerraformTree.Kind.TEMPLATE_STRING_PART_LITERAL)) {
+        highlight(tree, STRING);
+      } else if (tree.is(TerraformTree.Kind.NUMERIC_LITERAL)) {
+        highlight(tree, KEYWORD_LIGHT);
+      } else {
+        highlight(tree, CONSTANT);
+      }
+    });
+    register(AttributeTree.class, (ctx, tree) -> highlight(tree.key(), ANNOTATION));
+    register(ObjectElementTree.class, (ctx, tree) -> highlight(tree.key(), ANNOTATION));
   }
 
 }
