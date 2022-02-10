3ce773bc33e698809b75a7179ab2c9351dc01799
Stanislav <stanislav.jordanov@sonarsource.com>
2022-02-10 23:25:19 +0200

    use correct messages when reporting S6400

--- iac-extensions/terraform/src/test/resources/checks/GCP/AdministrativeRolesOnWorkloadResourcesCheck/role_assignment.tf
+++ iac-extensions/terraform/src/test/resources/checks/GCP/AdministrativeRolesOnWorkloadResourcesCheck/role_assignment.tf
@@ -1,25 +1,25 @@
 resource "google_apigee_environment_iam_binding" "binding" {
   org_id = google_apigee_environment.apigee_environment.org_id
   env_id = google_apigee_environment.apigee_environment.name
-  role  = "roles/ml.jobOwner" # Noncompliant
+  role  = "roles/ml.jobOwner" # Noncompliant {{Make sure it is safe to give those members full access to the resource.}}
   members = [
     "user:jane@example.com",
   ]
-}
-
-resource "google_apigee_environment_iam_member" "member" {
-  role = "roles/apigee.admin" # Noncompliant
-}
-
-resource "google_apigee_environment_iam_member" "member" {
-  role = "roles/apigee.AdMiN" # Noncompliant
 }
 
 resource "google_tags_tag_value_iam_binding" "binding2" {
   role  = "roles/ml.SuperUser.v2" # Noncompliant
 }
 
-resource "google_tags_tag_value_iam_member" "member2" {
+resource "google_apigee_environment_iam_member" "member" {
+  role = "roles/apigee.admin" # Noncompliant {{Make sure it is safe to grant that member full access to the resource.}}
+}
+
+resource "google_apigee_environment_iam_member" "member2" {
+  role = "roles/apigee.AdMiN" # Noncompliant
+}
+
+resource "google_tags_tag_value_iam_member" "member3" {
   role = "roles/tags.manager.v3" # Noncompliant
 }
 
@@ -29,7 +29,7 @@ resource "google_apigee_environment_iam_binding" "binding3" {
   role  = "roles/ml.dev" # Compliant
 }
 
-resource "google_apigee_environment_iam_member" "member3" {
+resource "google_apigee_environment_iam_member" "member4" {
   role = "roles/apigee.group" # Compliant
 }
 
@@ -37,10 +37,10 @@ resource "google_tags_tag_value_iam_binding_xyz" "binding4" {
   role  = "roles/ml.SuperUser.v2"
 }
 
-resource "google_tags_tag_value_iam_member_xyz" "member4" {
+resource "google_tags_tag_value_iam_member_xyz" "member5" {
   role = "roles/tags.manager.v3"
 }
 
-resource "google_tags_tag_value_iam_member" "member4" {
+resource "google_tags_tag_value_iam_member" "member6" {
   not_a_role = "roles/tags.ADMIN.v3"
 }
