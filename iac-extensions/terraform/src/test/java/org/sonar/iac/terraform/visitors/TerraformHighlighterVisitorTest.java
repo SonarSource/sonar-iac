0d365882f40ab62c355951705f0e42b4ba0538cf
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2022-02-10 16:33:12 +0100

    SONARIAC-370 Update Terraform syntax highlighting (#303)

--- iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/visitors/TerraformHighlighterVisitorTest.java
+++ iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/visitors/TerraformHighlighterVisitorTest.java
@@ -23,9 +23,11 @@ import org.junit.jupiter.api.Test;
 import org.sonar.iac.common.testing.AbstractHighlightingTest;
 import org.sonar.iac.terraform.parser.HclParser;
 
+import static org.sonar.api.batch.sensor.highlighting.TypeOfText.ANNOTATION;
 import static org.sonar.api.batch.sensor.highlighting.TypeOfText.COMMENT;
 import static org.sonar.api.batch.sensor.highlighting.TypeOfText.CONSTANT;
 import static org.sonar.api.batch.sensor.highlighting.TypeOfText.KEYWORD;
+import static org.sonar.api.batch.sensor.highlighting.TypeOfText.KEYWORD_LIGHT;
 import static org.sonar.api.batch.sensor.highlighting.TypeOfText.STRING;
 
 class TerraformHighlighterVisitorTest extends AbstractHighlightingTest {
@@ -66,22 +68,71 @@ class TerraformHighlighterVisitorTest extends AbstractHighlightingTest {
 
   @Test
   void block_type() {
-    highlight("block {}");
-    assertHighlighting(0, 4, KEYWORD);
-    assertHighlighting(5, 7, null);
+    highlight("block \"label\" {}");
+    assertHighlighting(0, 4, CONSTANT);
+    assertHighlighting(6, 12, KEYWORD);
+    assertHighlighting(13, 14, null);
   }
 
   @Test
   void string_literal() {
     highlight("a = \"abc\"");
-    assertHighlighting(0, 3, null);
+    assertHighlighting(0, 0, ANNOTATION);
+    assertHighlighting(1, 3, null);
     assertHighlighting(4, 8, STRING);
   }
 
   @Test
-  void non_string_literal() {
+  void numeric_literal() {
     highlight("a = 12");
-    assertHighlighting(0, 3, null);
+    assertHighlighting(0, 0, ANNOTATION);
+    assertHighlighting(1, 3, null);
+    assertHighlighting(4, 5, KEYWORD_LIGHT);
+  }
+
+  @Test
+  void boolean_literal() {
+    highlight("a = true");
+    assertHighlighting(0, 0, ANNOTATION);
+    assertHighlighting(1, 3, null);
     assertHighlighting(4, 5, CONSTANT);
+  }
+
+  @Test
+  void null_literal() {
+    highlight("a = null");
+    assertHighlighting(0, 0, ANNOTATION);
+    assertHighlighting(1, 3, null);
+    assertHighlighting(4, 5, CONSTANT);
+  }
+
+  @Test
+  void attribute_access() {
+    highlight("a = foo.bar");
+    assertHighlighting(0, 0, ANNOTATION);
+    assertHighlighting(1, 3, null);
+    assertHighlighting(4, 10, KEYWORD);
+  }
+
+  @Test
+  void object() {
+    highlight("tags = {git_commit = \"81738b80d571fa3034633690d13ffb460e1e7dea\"}");
+    assertHighlighting(0, 3, ANNOTATION);
+    assertHighlighting(4, 7, null);
+    assertHighlighting(8, 17, ANNOTATION);
+    assertHighlighting(18, 19, null);
+    assertHighlighting(21, 62, STRING);
+    assertHighlighting(63, 64, null);
+  }
+
+  @Test
+  void template_expression() {
+    highlight("name = \"terragoat-app-service-${var.environment}${random_integer.rnd_int.result}\"");
+    assertHighlighting(0, 3, ANNOTATION);
+    assertHighlighting(4, 7, null);
+    assertHighlighting(8, 29, STRING);
+    assertHighlighting(30, 31, null);
+    assertHighlighting(32, 46, KEYWORD);
+    assertHighlighting(47, 47, null);
   }
 }
