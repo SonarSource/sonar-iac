631b0e6abfbb3d310cdeb3407a47e98fde055467
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2022-02-14 13:36:46 +0100

    Extract Terraform Symbols from Check (#307)
    * Extract Terraform Symbols from Check
    
    * Address review suggestions

---
+++ iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/symbols/AttributeSymbolTest.java
@@ -0,0 +1,79 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.iac.terraform.symbols;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.iac.terraform.api.tree.AttributeTree;
+
+class AttributeSymbolTest extends AbstractSymbolTest{
+
+  @Test
+  void report_fromPresent() {
+    AttributeTree tree = parseAttribute("my_attribute = 1");
+    AttributeSymbol attribute = AttributeSymbol.fromPresent(ctx, tree, parentBlock);
+    attribute.report("message");
+    assertIssueReported(tree, "message");
+  }
+
+  @Test
+  void report_fromAbsent() {
+    AttributeSymbol attribute = AttributeSymbol.fromAbsent(ctx, "my_attribute", parentBlock);
+    attribute.report("message");
+    assertNoIssueReported();
+  }
+
+  @Test
+  void reportIfAbsent_fromAbsent() {
+    AttributeSymbol attribute = AttributeSymbol.fromAbsent(ctx, "my_attribute", parentBlock);
+    attribute.reportIfAbsent("%s");
+    assertIssueReported(parentBlock.tree.key(), "my_attribute");
+  }
+
+  @Test
+  void reportIfAbsent_fromPresent() {
+    AttributeTree tree = parseAttribute("my_attribute = 1");
+    AttributeSymbol attribute = AttributeSymbol.fromPresent(ctx, tree, parentBlock);
+    attribute.reportIfAbsent("%s");
+    assertNoIssueReported();
+  }
+
+  @Test
+  void reportIf_fromPresent() {
+    AttributeTree tree = parseAttribute("my_attribute = 1");
+    AttributeSymbol attribute = AttributeSymbol.fromPresent(ctx, tree, parentBlock);
+    attribute.reportIf(e -> true, "message");
+    assertIssueReported(tree, "message");
+  }
+
+  @Test
+  void reportIf_fromAbsent() {
+    AttributeSymbol attribute = AttributeSymbol.fromAbsent(ctx, "my_attribute", parentBlock);
+    attribute.reportIf(e -> true, "message");
+    assertNoIssueReported();
+  }
+
+  @Test
+  void reportIf_not_matching_predicate() {
+    AttributeTree tree = parseAttribute("my_attribute = 1");
+    AttributeSymbol attribute = AttributeSymbol.fromPresent(ctx, tree, parentBlock);
+    attribute.reportIf(e -> false, "message");
+    assertNoIssueReported();
+  }
+}
---
+++ iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/symbols/ResourceSymbolTest.java
@@ -0,0 +1,65 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.iac.terraform.symbols;
+
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.sonar.iac.terraform.api.tree.BlockTree;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ResourceSymbolTest extends AbstractSymbolTest {
+
+  @Test
+  void report() {
+    BlockTree tree = parseBlock("resource \"my_type\" \"my_name\" {}");
+    ResourceSymbol resource = ResourceSymbol.fromPresent(ctx, tree);
+    resource.report("message");
+    assertIssueReported(tree.labels().get(0), "message");
+  }
+
+  @Test
+  void reportIfAbsent() {
+    BlockTree tree = parseBlock("resource \"my_type\" \"my_name\" {}");
+    ResourceSymbol resource = ResourceSymbol.fromPresent(ctx, tree);
+    assertThatThrownBy(() -> resource.reportIfAbsent("message"))
+      .isInstanceOf(UnsupportedOperationException.class)
+      .hasMessage("Resource symbols should always exists");
+  }
+
+  @Test
+  void reportIfAbsent_with_resource_as_parent() {
+    BlockTree resourceTree = parseBlock("resource \"my_type\" \"my_name\" {}");
+    BlockSymbol resource = ResourceSymbol.fromPresent(ctx, resourceTree);
+    BlockSymbol child = BlockSymbol.fromAbsent(ctx, "missing_block", resource);
+    child.reportIfAbsent("%s");
+    assertIssueReported(resourceTree.labels().get(0), "missing_block");
+  }
+
+  @Test
+  void create_incomplete_resource() {
+    BlockTree tree = parseBlock("resource {}");
+    ResourceSymbol resource = ResourceSymbol.fromPresent(ctx, tree);
+    assertThat(resource.type).isEmpty();
+    resource.report("message", Collections.emptyList());
+    assertNoIssueReported();
+  }
+}
