c2425aa871c136c7663c5f6e242c0384c4efb6fa
Stanislav <90248579+stanislav-jordanov-sonarsource@users.noreply.github.com>
2022-02-18 19:02:44 +0200

    SONARIAC-360 Rule 6400: Granting highly privileged GCP resource rights is security-sensitive (IAM Policy) (#315)

--- iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/gcp/HighPrivilegedRolesOnWorkloadResourcesCheck.java
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/gcp/HighPrivilegedRolesOnWorkloadResourcesCheck.java
@@ -20,8 +20,15 @@
 package org.sonar.iac.terraform.checks.gcp;
 
 import java.util.List;
+import java.util.Set;
 import org.sonar.check.Rule;
+import org.sonar.iac.common.api.checks.CheckContext;
+import org.sonar.iac.common.api.checks.InitContext;
+import org.sonar.iac.common.extension.visitors.TreeContext;
+import org.sonar.iac.terraform.api.tree.BlockTree;
+import org.sonar.iac.terraform.api.tree.FileTree;
 import org.sonar.iac.terraform.checks.AbstractNewResourceCheck;
+import org.sonar.iac.terraform.symbols.ResourceSymbol;
 
 import static org.sonar.iac.terraform.checks.utils.ExpressionPredicate.equalTo;
 import static org.sonar.iac.terraform.checks.utils.ExpressionPredicate.matchesPattern;
@@ -32,16 +39,27 @@ public class HighPrivilegedRolesOnWorkloadResourcesCheck extends AbstractNewReso
   private static final String MESSAGE_FOR_BINDING = "Make sure it is safe to give those members full access to the resource.";
   private static final String MESSAGE_FOR_MEMBER = "Make sure it is safe to grant that member full access to the resource.";
   private static final String MESSAGE_ON_GRANT_FULL_ACCESS = "Make sure it is safe to grant full access to the resource.";
+  private static final String SECONDARY_MESSAGE = "The policy is used here.";
+
+  private static final String SENSITIVE_ROLES = ".*(?:admin|manager|owner|superuser).*";
+
+  private final PolicyReferenceCollector collector = new PolicyReferenceCollector(IAM_POLICY_RESOURCE_TYPES);
+
+  @Override
+  public void initialize(InitContext init) {
+    super.initialize(init);
+    init.register(FileTree.class, (ctx, tree) -> collector.scan(new TreeContext(), tree));
+  }
 
   @Override
   protected void registerResourceConsumer() {
     register(List.of(IAM_BINDING_RESOURCE_NAMES),
       resource -> resource.attribute("role")
-        .reportIf(matchesPattern(".*(?:ADMIN|MANAGER|OWNER|SUPERUSER).*"), MESSAGE_FOR_BINDING));
+        .reportIf(matchesPattern(SENSITIVE_ROLES), MESSAGE_FOR_BINDING));
 
     register(List.of(IAM_MEMBER_RESOURCE_NAMES),
       resource -> resource.attribute("role")
-        .reportIf(matchesPattern(".*(?:ADMIN|MANAGER|OWNER|SUPERUSER).*"), MESSAGE_FOR_MEMBER));
+        .reportIf(matchesPattern(SENSITIVE_ROLES), MESSAGE_FOR_MEMBER));
 
     register("google_cloud_identity_group",
       resource -> resource.blocks("roles").forEach(
@@ -62,6 +80,15 @@ public class HighPrivilegedRolesOnWorkloadResourcesCheck extends AbstractNewReso
         "google_storage_object_acl"),
       resource -> resource.list("role_entity")
         .reportItemIf(matchesPattern("OWNER:.*"), MESSAGE_ON_GRANT_FULL_ACCESS));
+  }
+
+  @Override
+  protected void provideResource(CheckContext ctx, BlockTree blockTree) {
+    super.provideResource(ctx, blockTree);
+    if (isDataOfType(blockTree, "google_iam_policy")) {
+      ResourceSymbol dataData = ResourceSymbol.fromPresent(ctx, blockTree);
+      collector.checkPolicy(dataData, matchesPattern(SENSITIVE_ROLES), MESSAGE_FOR_BINDING, SECONDARY_MESSAGE);
+    }
   }
 
   private static final String[] IAM_BINDING_RESOURCE_NAMES = {
@@ -181,6 +208,67 @@ public class HighPrivilegedRolesOnWorkloadResourcesCheck extends AbstractNewReso
     "google_spanner_instance_iam_member",
     "google_storage_bucket_iam_member",
     "google_tags_tag_key_iam_member",
-    "google_tags_tag_value_iam_member",
+    "google_tags_tag_value_iam_member"
   };
+  
+  private static final String[] IAM_POLICY_RESOURCE_NAMES = {
+    "google_apigee_environment_iam_policy",
+    "google_api_gateway_api_config_iam_policy",
+    "google_api_gateway_api_iam_policy",
+    "google_api_gateway_gateway_iam_policy",
+    "google_artifact_registry_repository_iam_policy",
+    "google_bigquery_dataset_iam_policy",
+    "google_bigquery_table_iam_policy",
+    "google_bigtable_instance_iam_policy",
+    "google_bigtable_table_iam_policy",
+    "google_billing_account_iam_policy",
+    "google_binary_authorization_attestor_iam_policy",
+    "google_cloudfunctions_function_iam_policy",
+    "google_cloud_run_service_iam_policy",
+    "google_compute_disk_iam_policy",
+    "google_compute_image_iam_policy",
+    "google_compute_instance_iam_policy",
+    "google_compute_machine_image_iam_policy",
+    "google_compute_region_disk_iam_policy",
+    "google_compute_subnetwork_iam_policy",
+    "google_dataproc_cluster_iam_policy",
+    "google_dataproc_job_iam_policy",
+    "google_data_catalog_entry_group_iam_policy",
+    "google_data_catalog_policy_tag_iam_policy",
+    "google_data_catalog_tag_template_iam_policy",
+    "google_data_catalog_taxonomy_iam_policy",
+    "google_endpoints_service_iam_policy",
+    "google_kms_crypto_key_iam_policy",
+    "google_kms_key_ring_iam_policy",
+    "google_healthcare_consent_store_iam_policy",
+    "google_healthcare_dataset_iam_policy",
+    "google_healthcare_dicom_store_iam_policy",
+    "google_healthcare_fhir_store_iam_policy",
+    "google_healthcare_hl7_v2_store_iam_policy",
+    "google_iap_app_engine_service_iam_policy",
+    "google_iap_app_engine_version_iam_policy",
+    "google_iap_tunnel_iam_policy",
+    "google_iap_tunnel_instance_iam_policy",
+    "google_iap_web_backend_service_iam_policy",
+    "google_iap_web_iam_policy",
+    "google_iap_web_type_app_engine_iam_policy",
+    "google_iap_web_type_compute_iam_policy",
+    "google_notebooks_instance_iam_policy",
+    "google_notebooks_runtime_iam_policy",
+    "google_privateca_ca_pool_iam_policy",
+    "google_pubsub_subscription_iam_policy",
+    "google_pubsub_topic_iam_policy",
+    "google_runtimeconfig_config_iam_policy",
+    "google_secret_manager_secret_iam_policy",
+    "google_service_directory_namespace_iam_policy",
+    "google_service_directory_service_iam_policy",
+    "google_sourcerepo_repository_iam_policy",
+    "google_spanner_database_iam_policy",
+    "google_spanner_instance_iam_policy",
+    "google_storage_bucket_iam_policy",
+    "google_tags_tag_key_iam_policy",
+    "google_tags_tag_value_iam_policy"
+  };
+
+  private static final Set<String> IAM_POLICY_RESOURCE_TYPES = Set.of(IAM_POLICY_RESOURCE_NAMES);
 }
---
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/gcp/PolicyReferenceCollector.java
@@ -0,0 +1,85 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.iac.terraform.checks.gcp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
+import org.sonar.iac.common.api.checks.SecondaryLocation;
+import org.sonar.iac.common.checks.PropertyUtils;
+import org.sonar.iac.common.extension.visitors.TreeContext;
+import org.sonar.iac.common.extension.visitors.TreeVisitor;
+import org.sonar.iac.terraform.api.tree.AttributeAccessTree;
+import org.sonar.iac.terraform.api.tree.AttributeTree;
+import org.sonar.iac.terraform.api.tree.BlockTree;
+import org.sonar.iac.terraform.api.tree.ExpressionTree;
+import org.sonar.iac.terraform.symbols.ResourceSymbol;
+
+import static org.sonar.iac.terraform.api.tree.TerraformTree.Kind.ATTRIBUTE_ACCESS;
+import static org.sonar.iac.terraform.checks.AbstractNewResourceCheck.isResource;
+import static org.sonar.iac.terraform.checks.AbstractNewResourceCheck.resourceType;
+import static org.sonar.iac.terraform.checks.utils.TerraformUtils.attributeAccessToString;
+
+class PolicyReferenceCollector extends TreeVisitor<TreeContext> {
+  private final Map<String, List<AttributeTree>> policyReferences = new HashMap<>();
+  private final Set<String> relevantResources;
+
+  public PolicyReferenceCollector(Set<String> relevantResourceTypes) {
+    relevantResources = relevantResourceTypes;
+    register(BlockTree.class, (ctx, tree) -> {
+      if (isResource(tree) && relevantResources.contains(resourceType(tree))) {
+        collectReference(tree);
+      }
+    });
+  }
+
+  private void collectReference(BlockTree tree) {
+    PropertyUtils.get(tree, "policy_data", AttributeTree.class)
+      .filter(policyData -> policyData.value().is(ATTRIBUTE_ACCESS))
+      .ifPresent(policyData -> {
+        String key = attributeAccessToString((AttributeAccessTree) policyData.value());
+        policyReferences.computeIfAbsent(key, s -> new ArrayList<>()).add(policyData);
+      });
+  }
+
+  private List<AttributeTree> getReferrersList(String policyDataName) {
+    List<AttributeTree> res = policyReferences.get(String.format("data.google_iam_policy.%s.policy_data", policyDataName));
+    return res == null ? Collections.emptyList() : res;
+  }
+
+  public void checkPolicy(ResourceSymbol data, Predicate<ExpressionTree> isSensitiveRole, String primaryMsg, String secondaryMsg) {
+    List<AttributeTree> referrers = getReferrersList(data.name);
+    if (referrers.isEmpty()) {
+      return;
+    }
+
+    SecondaryLocation[] secondary = referrers.stream()
+      .map(referrer -> new SecondaryLocation(referrer, secondaryMsg))
+      .toArray(SecondaryLocation[]::new);
+
+    data.blocks("binding").forEach(
+      binding -> binding.attribute("role")
+        .reportIf(isSensitiveRole, primaryMsg, secondary));
+  }
+}
