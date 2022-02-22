46d50acd1de3b1ec6350720d812a72b47c4f4e86
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2022-02-22 08:56:49 +0100

    SONARIAC-312 Add assertion on issue message

--- iac-extensions/terraform/src/test/resources/checks/CryptoKeyRotationPeriod/gcp.tf
+++ iac-extensions/terraform/src/test/resources/checks/CryptoKeyRotationPeriod/gcp.tf
@@ -4,7 +4,7 @@ resource "google_kms_crypto_key" "k1" {
   rotation_period = "100000s"
 }
 
-resource "google_kms_crypto_key" "k2" { # Noncompliant
+resource "google_kms_crypto_key" "k2" { # Noncompliant {{Make sure creating a key without a rotation period is safe here.}}
   name            = "k2"
   key_ring        = google_kms_key_ring.keyring.id
 }
