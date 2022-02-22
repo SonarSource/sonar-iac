e95e347a1a956841bf6bb2e5c64554da369d9a32
Stanislav <stanislav.jordanov@sonarsource.com>
2022-02-22 13:06:12 +0200

    update TerraformCheckList

--- iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/TerraformCheckList.java
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/TerraformCheckList.java
@@ -33,6 +33,7 @@ import org.sonar.iac.terraform.checks.azure.SubscriptionRoleAssignmentCheck;
 import org.sonar.iac.terraform.checks.gcp.AttributeBasedAccessControlCheck;
 import org.sonar.iac.terraform.checks.gcp.CryptoKeyRotationPeriodCheck;
 import org.sonar.iac.terraform.checks.gcp.CustomRoleCheck;
+import org.sonar.iac.terraform.checks.gcp.DnsZoneMissingDnssecCheck;
 import org.sonar.iac.terraform.checks.gcp.ExcessivePermissionsCheck;
 import org.sonar.iac.terraform.checks.gcp.HighPrivilegedRolesOnWorkloadResourcesCheck;
 import org.sonar.iac.terraform.checks.gcp.PublicAccessCheck;
@@ -87,6 +88,7 @@ public class TerraformCheckList {
 
       // GCP
       AttributeBasedAccessControlCheck.class,
+      DnsZoneMissingDnssecCheck.class,
       CryptoKeyRotationPeriodCheck.class,
       CustomRoleCheck.class,
       ExcessivePermissionsCheck.class,
