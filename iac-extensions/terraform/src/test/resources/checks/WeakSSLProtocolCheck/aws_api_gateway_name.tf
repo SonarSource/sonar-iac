05e2335a76f987092c1dffd6e15a873465a82d7f
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2022-03-14 15:42:16 +0100

    SONARIAC-379 S4423: Update issue message (#344)

--- iac-extensions/terraform/src/test/resources/checks/WeakSSLProtocolCheck/aws_api_gateway_name.tf
+++ iac-extensions/terraform/src/test/resources/checks/WeakSSLProtocolCheck/aws_api_gateway_name.tf
@@ -1,10 +1,10 @@
 resource "aws_api_gateway_domain_name" "weak_ssl_protocol" {
   domain_name = "api.example.com"
-  security_policy = "TLS_1_0" # Noncompliant {{Change this configuration to use a stronger protocol.}}
+  security_policy = "TLS_1_0" # Noncompliant {{Change this code to disable support of older TLS versions.}}
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
 
-# Noncompliant@+1 {{Omitting security_policy disables traffic encryption. Make sure it is safe here.}}
+# Noncompliant@+1 {{Set "security_policy" to disable support of older TLS versions.}}
 resource "aws_api_gateway_domain_name" "no_security_policy" {
   #      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   domain_name = "api.example.com"
--- iac-extensions/terraform/src/test/resources/checks/WeakSSLProtocolCheck/gcp_compute_ssl_policy.tf
+++ iac-extensions/terraform/src/test/resources/checks/WeakSSLProtocolCheck/gcp_compute_ssl_policy.tf
@@ -1,5 +1,5 @@
 resource "google_compute_ssl_policy" "tls11-ssl-policy" {
-  min_tls_version = "TLS_1_1" # Noncompliant {{Change this configuration to use a stronger protocol.}}
+  min_tls_version = "TLS_1_1" # Noncompliant {{Change this code to disable support of older TLS versions.}}
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
 
@@ -16,7 +16,7 @@ resource "google_compute_ssl_policy" "tls11-ssl-policy" {
   min_tls_version = var.ssl.tls-version
 }
 
-# Noncompliant@+1 {{Omitting min_tls_version disables traffic encryption. Make sure it is safe here.}}
+# Noncompliant@+1 {{Set "min_tls_version" to disable support of older TLS versions.}}
 resource "google_compute_ssl_policy" "default-tls-version-ssl-policy" {
       #  ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
