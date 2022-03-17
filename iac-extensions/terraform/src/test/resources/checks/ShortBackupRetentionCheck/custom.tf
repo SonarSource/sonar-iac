ccb5c9769d4f83a068825f08a684a679c37fef15
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2022-03-17 16:47:10 +0100

    SONARIAC-404 S6364: Update detection logic when backup retention is undefined (#360)

--- iac-extensions/terraform/src/test/resources/checks/ShortBackupRetentionCheck/custom.tf
+++ iac-extensions/terraform/src/test/resources/checks/ShortBackupRetentionCheck/custom.tf
@@ -1,18 +1,13 @@
 resource "aws_db_instance" "too_low_period" {
   name                 = "non_compliant_db"
-  backup_retention_period = 2
+  backup_retention_period = 1
 }
 
-resource "aws_db_instance" "missing_period" { # Noncompliant
-  #      ^^^^^^^^^^^^^^^^^
+resource "aws_db_instance" "missing_period" {
   name                 = "non_compliant_db"
 }
 
 resource "aws_rds_cluster" "too_low_period" {
   name                 = "non_compliant_db"
-  backup_retention_period = 2
-}
-
-resource "aws_rds_cluster" "missing_period" { # Noncompliant
-  name                 = "non_compliant_db"
+  backup_retention_period = 0 # Noncompliant
 }
