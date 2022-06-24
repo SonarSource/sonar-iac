9ad4d160ea78a39cbe1254976071ebea7f407409
Nils Werner <nils.werner@sonarsource.com>
2022-06-24 10:55:30 +0200

    SONARIAC-421 Add AnalysisWarnings for TerraformProviders

--- iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/plugin/TerraformProvidersTest.java
+++ iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/plugin/TerraformProvidersTest.java
@@ -26,15 +26,20 @@ import org.junit.jupiter.api.io.TempDir;
 import org.sonar.api.batch.sensor.SensorContext;
 import org.sonar.api.batch.sensor.internal.SensorContextTester;
 import org.sonar.api.config.internal.MapSettings;
+import org.sonar.api.notifications.AnalysisWarnings;
 import org.sonar.api.utils.Version;
 import org.sonar.api.utils.log.LogTesterJUnit5;
 import org.sonar.api.utils.log.LoggerLevel;
 import org.sonar.iac.terraform.plugin.TerraformProviders.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 class TerraformProvidersTest {
 
+  private AnalysisWarnings analysisWarnings = spy(AnalysisWarnings.class);
 
   private static final String AWS_KEY = "sonar.terraform.provider.aws.version";
   @TempDir
@@ -44,26 +49,48 @@ class TerraformProvidersTest {
   public LogTesterJUnit5 logTester = new LogTesterJUnit5();
 
   @Test
-  void parse_valid_version() {
-    TerraformProviders providerVersions = new TerraformProviders(context(AWS_KEY, "1.3.4"));
-    Provider provider =  providerVersions.provider(Provider.Identifier.AWS);
+  void provider_with_valid_version() {
+    TerraformProviders providers = providers(context(AWS_KEY, "1.3.4"));
+    Provider provider =  providers.provider(Provider.Identifier.AWS);
     assertThat(provider.providerVersion).isEqualTo(Version.parse("1.3.4"));
   }
 
   @Test
-  void parse_invalid_version() {
-    TerraformProviders providerVersions = new TerraformProviders(context(AWS_KEY, "v1.3.4"));
-    Provider provider =  providerVersions.provider(Provider.Identifier.AWS);
+  void provider_with_invalid_version() {
+    TerraformProviders providers = providers(context(AWS_KEY, "v1.3.4"));
+    Provider provider =  providers.provider(Provider.Identifier.AWS);
+
     assertThat(provider.providerVersion).isNull();
     assertThat(logTester.logs(LoggerLevel.WARN))
       .containsExactly("Can not parse provider version \"sonar.terraform.provider.aws.version\".");
+    verify(analysisWarnings, times(1))
+      .addUnique("Can not parse provider version for \"sonar.terraform.provider.aws.version\". Please check format.");
   }
 
   @Test
-  void parse_empty_version() {
-    TerraformProviders providerVersions = new TerraformProviders(context(AWS_KEY, ""));
-    Provider provider =  providerVersions.provider(Provider.Identifier.AWS);
+  void provider_with_empty_version() {
+    TerraformProviders providers = providers(context(AWS_KEY, ""));
+    Provider provider =  providers.provider(Provider.Identifier.AWS);
     assertThat(provider.providerVersion).isNull();
+  }
+
+  @Test
+  void provider_without_provided_version() {
+    TerraformProviders providers = providers(SensorContextTester.create(baseDir));
+    providers.provider(Provider.Identifier.AWS);
+    verify(analysisWarnings, times(1))
+      .addUnique("Provide the used AWS provider version via the \"sonar.terraform.provider.aws.version\" " +
+        "property to increase the accuracy of your results.");
+  }
+
+  @Test
+  void single_warning_for_missing_provided_version() {
+    TerraformProviders providers = providers(SensorContextTester.create(baseDir));
+    providers.provider(Provider.Identifier.AWS);
+    providers.provider(Provider.Identifier.AWS);
+    verify(analysisWarnings, times(1))
+      .addUnique("Provide the used AWS provider version via the \"sonar.terraform.provider.aws.version\" " +
+        "property to increase the accuracy of your results.");
   }
 
   @Test
@@ -80,6 +107,10 @@ class TerraformProvidersTest {
   void assess_without_provider_version() {
     Provider provider = new Provider(null);
     assertThat(provider.hasVersionLowerThan(Version.create(2, 0))).isFalse();
+  }
+
+  private TerraformProviders providers(SensorContext context) {
+    return new TerraformProviders(context, analysisWarnings);
   }
 
   private SensorContext context(String key, String value) {
