2d0722b239b6dbadb2356576ea21eb9bdb54c181
Nils Werner <nils.werner@sonarsource.com>
2022-07-13 15:54:05 +0200

    SONARIAC-91 Add complete string parsing validation in TerraformTreeModelTest

--- iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/tree/impl/TerraformTreeModelTest.java
+++ iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/tree/impl/TerraformTreeModelTest.java
@@ -19,7 +19,13 @@
  */
 package org.sonar.iac.terraform.tree.impl;
 
+import com.sonar.sslr.api.RecognitionException;
 import com.sonar.sslr.api.typed.ActionParser;
+import org.sonar.api.batch.fs.TextPointer;
+import org.sonar.iac.common.api.tree.HasTextRange;
+import org.sonar.iac.common.extension.visitors.TreeContext;
+import org.sonar.iac.common.extension.visitors.TreeVisitor;
+import org.sonar.iac.terraform.api.tree.SyntaxToken;
 import org.sonar.iac.terraform.api.tree.TerraformTree;
 import org.sonar.iac.terraform.parser.HclParser;
 import org.sonar.sslr.grammar.GrammarRuleKey;
@@ -37,10 +43,78 @@ public abstract class TerraformTreeModelTest {
   protected <T extends TerraformTree> T parse(String s, GrammarRuleKey rootRule) {
     p = new HclParser(rootRule);
     TerraformTree node = p.parse(s);
-
-    //TODO: SONARIAC-91 similar to PHP parser, check how far we have parsed. Missing possibility to represent trees as string for now
-    //checkFullFidelity(node, s.trim());
+    checkFullFidelity(node, s.trim());
 
     return (T) node;
+  }
+
+  /**
+   * Return the concatenation of all the given node tokens value.
+   */
+  protected static String expressionToString(TerraformTree node) {
+    SourceBuilder writer = new SourceBuilder();
+    return writer.build(node).trim();
+  }
+
+  private static void checkFullFidelity(TerraformTree tree, String inputString) {
+    String resultString = expressionToString(tree);
+    if (!inputString.equals(resultString)) {
+      String message;
+      if (inputString.startsWith(resultString)) {
+        message = "Only beginning of the input string is parsed: " + resultString;
+      } else {
+        message = "Some tokens are lost. See result tree string: " + resultString;
+      }
+      throw new RecognitionException(0, message);
+    }
+  }
+
+  static class SourceBuilder extends TreeVisitor<TreeContext> {
+
+    private final StringBuilder stringBuilder = new StringBuilder();
+    private int line = 1;
+    private int column = 0;
+
+    public String build(TerraformTree tree) {
+      register(SyntaxToken.class, (ctx, token) -> {
+        token.comments().forEach(comment -> appendText(comment, comment.value()));
+        appendText(token, token.value());
+      });
+      scan(new TreeContext(), tree);
+      return stringBuilder.toString();
+    }
+
+    private void appendText(HasTextRange token, String text) {
+      insertMissingSpaceBefore(token.textRange().start());
+      stringBuilder.append(text);
+      String[] lines = text.split("\r\n|\n|\r", -1);
+      if (lines.length > 1) {
+        line += lines.length - 1;
+        column = lines[lines.length - 1].length();
+      } else {
+        column += text.length();
+      }
+    }
+
+    private void insertMissingSpaceBefore(TextPointer startPointer) {
+
+      int linesToInsert = startPointer.line() - line;
+      if (linesToInsert < 0) {
+        throw new IllegalStateException("Illegal token line for " + startPointer.lineOffset());
+      } else if (linesToInsert > 0) {
+        for (int i = 0; i < linesToInsert; i++) {
+          stringBuilder.append("\n");
+          line++;
+        }
+        column = 0;
+      }
+      int spacesToInsert = startPointer.lineOffset() - column;
+      for (int i = 0; i < spacesToInsert; i++) {
+        stringBuilder.append(' ');
+        column++;
+      }
+    }
+
+
   }
 }
