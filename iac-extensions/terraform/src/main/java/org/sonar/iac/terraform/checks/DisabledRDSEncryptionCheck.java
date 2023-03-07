1737e1366251ee6bbc04f06bb88d139d20df0232
Navendu Barua <navendu.barua@sonarsource.com>
2023-03-07 12:38:10 +0100

    SONARIAC-438 modified check for engine_mode and updated for db-backup-replication

--- iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/DisabledRDSEncryptionCheck.java
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/DisabledRDSEncryptionCheck.java
@@ -32,14 +32,18 @@ import static org.sonar.iac.terraform.checks.utils.ExpressionPredicate.notEqualT
 @Rule(key = "S6303")
 public class DisabledRDSEncryptionCheck extends AbstractNewResourceCheck {
 
-  private static final String MESSAGE = "Make sure that using unencrypted RDS DB Instances is safe here.";
+  private static final String MESSAGE_DB_INSTANCE = "Make sure that using unencrypted RDS DB Instances is safe here.";
+  private static final String MESSAGE_RDS_CLUSTER = "Make sure that using an unencrypted RDS DB Cluster is safe here.";
+  private static final String MESSAGE_DB_INSTANCE_BACKUP_REPLICATION = "Make sure that using an unencrypted DB backup replication is safe here.";
   private static final String OMITTING_MESSAGE = "Omitting \"storage_encrypted\" disables databases encryption. Make sure it is safe here.";
-  private static final String SECONDARY_MESSAGE = "Related RDS DBInstance";
+  private static final String SECONDARY_MESSAGE_DB_INSTANCE = "Related RDS DB Instance";
+  private static final String SECONDARY_MESSAGE_RDS_CLUSTER = "Related RDS Cluster";
   private static final List<String> EXCLUDE_AURORA_ATTRIBUTE = List.of("aurora", "aurora-mysql", "aurora-postgresql");
 
   protected void registerResourceConsumer() {
     register("aws_db_instance", DisabledRDSEncryptionCheck::checkAwsDbInstance);
     register("aws_rds_cluster", DisabledRDSEncryptionCheck::checkAwsRdsCluster);
+    register("aws_db_instance_automated_backups_replication", DisabledRDSEncryptionCheck::checkAwsDbInstanceBackupReplication);
   }
 
   private static void checkAwsDbInstance(ResourceSymbol resource) {
@@ -48,17 +52,22 @@ public class DisabledRDSEncryptionCheck extends AbstractNewResourceCheck {
       return;
     }
     resource.attribute("storage_encrypted")
-      .reportIf(isFalse(), MESSAGE, resource.toSecondary(SECONDARY_MESSAGE))
+      .reportIf(isFalse(), MESSAGE_DB_INSTANCE, resource.toSecondary(SECONDARY_MESSAGE_DB_INSTANCE))
       .reportIfAbsent(OMITTING_MESSAGE);
   }
 
   private static void checkAwsRdsCluster(ResourceSymbol resource) {
-    AttributeSymbol engine = resource.attribute("engine");
+    AttributeSymbol engine_mode = resource.attribute("engine_mode");
     AttributeSymbol storageEncrypted = resource.attribute("storage_encrypted");
-    storageEncrypted.reportIf(isFalse(), "Make sure that using an unencrypted RDS DB Cluster is safe here.", resource.toSecondary(SECONDARY_MESSAGE));
+    storageEncrypted.reportIf(isFalse(), MESSAGE_RDS_CLUSTER, resource.toSecondary(SECONDARY_MESSAGE_RDS_CLUSTER));
 
-    if (engine.is(notEqualTo("serverless"))) {
+    if (engine_mode.isAbsent() || engine_mode.is(notEqualTo("serverless"))) {
       storageEncrypted.reportIfAbsent(OMITTING_MESSAGE);
     }
+  }
+
+  private static void checkAwsDbInstanceBackupReplication(ResourceSymbol resource) {
+    resource.attribute("kms_key_id")
+      .reportIfAbsent(MESSAGE_DB_INSTANCE_BACKUP_REPLICATION);
   }
 }
