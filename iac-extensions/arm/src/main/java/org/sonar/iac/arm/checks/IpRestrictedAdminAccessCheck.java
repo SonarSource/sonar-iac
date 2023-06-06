f9c6b1d35f536a211c407db3279ffbe267278bdc
Rudy Regazzoni <rudy.regazzoni@sonarsource.com>
2023-06-06 11:31:27 +0200

    SONARIAC-843 Rule S6321 ARM: Add exceptions to simplified positive cases

--- iac-extensions/arm/src/main/java/org/sonar/iac/arm/checks/IpRestrictedAdminAccessCheck.java
+++ iac-extensions/arm/src/main/java/org/sonar/iac/arm/checks/IpRestrictedAdminAccessCheck.java
@@ -20,6 +20,9 @@
 package org.sonar.iac.arm.checks;
 
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.sonar.check.Rule;
 import org.sonar.iac.arm.tree.api.ArrayExpression;
 import org.sonar.iac.arm.tree.api.ResourceDeclaration;
@@ -35,18 +38,21 @@ public class IpRestrictedAdminAccessCheck implements IacCheck {
   private static final String MESSAGE = "Restrict IP addresses authorized to access administration services.";
   private static final String RESOURCE_TYPE = "Microsoft.Network/networkSecurityGroups/securityRules";
   private static final Set<String> SOURCE_ADDRESS_PREFIX_SENSITIVE = Set.of("*", "0.0.0.0/0", "::/0", "Internet");
+  private static final Set<String> SENSITIVE_PROTOCOL = Set.of("*", "TCP");
+  private static final Set<String> SENSITIVE_PORT = Set.of("*", "22", "3389");
+  private static final Pattern patternPortRange = Pattern.compile("^(?<min>\\d+)-(?<max>\\d+)$");
 
   @Override
   public void initialize(InitContext init) {
     init.register(ResourceDeclaration.class, (ctx, resource) -> {
       if (RESOURCE_TYPE.equals(resource.type().value())) {
         PropertyUtils.get(resource, "sourceAddressPrefix").ifPresent(propertyTree -> {
-          if (isSensitiveSourceAddress(propertyTree.value())) {
+          if (isSensitiveSourceAddress(propertyTree.value()) && !isResourceException(resource)) {
             ctx.reportIssue(propertyTree, MESSAGE);
           }
         });
         PropertyUtils.get(resource, "sourceAddressPrefixes").ifPresent(propertyTree -> {
-          if (containsSensitiveSourceAddress(propertyTree.value())) {
+          if (isArrayWith(propertyTree.value(), IpRestrictedAdminAccessCheck::isSensitiveSourceAddress) && !isResourceException(resource)) {
             ctx.reportIssue(propertyTree, MESSAGE);
           }
         });
@@ -58,11 +64,44 @@ public class IpRestrictedAdminAccessCheck implements IacCheck {
     return TextUtils.matchesValue(value, SOURCE_ADDRESS_PREFIX_SENSITIVE::contains).isTrue();
   }
 
-  private static boolean containsSensitiveSourceAddress(Tree value) {
-    if (value instanceof ArrayExpression) {
-      ArrayExpression array = (ArrayExpression) value;
+  private static boolean isResourceException(ResourceDeclaration resource) {
+    return PropertyUtils.valueStringIs(resource, "direction", str -> !"Inbound".equals(str))
+      || PropertyUtils.valueStringIs(resource, "access", str -> !"Allow".equals(str))
+      || PropertyUtils.valueStringIs(resource, "protocol", str -> !SENSITIVE_PROTOCOL.contains(str))
+      || PropertyUtils.valueIs(resource, "destinationPortRange", tree -> !isSensitivePort(tree))
+      || PropertyUtils.valueIs(resource, "destinationPortRanges", tree -> !isArrayWith(tree, IpRestrictedAdminAccessCheck::isSensitivePort));
+  }
+
+  private static boolean isSensitivePort(Tree tree) {
+    return TextUtils.getValue(tree)
+      .filter(value -> {
+        if (SENSITIVE_PORT.contains(value)) {
+          return true;
+        }
+        return isSensitivePortRange(value);
+      })
+      .isPresent();
+  }
+
+  private static boolean isSensitivePortRange(String portRange) {
+    Matcher matcher = patternPortRange.matcher(portRange);
+    if (matcher.find()) {
+      int min = Integer.parseInt(matcher.group("min"));
+      int max = Integer.parseInt(matcher.group("max"));
+      return inRange(22, min, max) || inRange(3389, min, max);
+    }
+    return false;
+  }
+
+  private static boolean inRange(int number, int min, int max) {
+    return number >= min && number <= max;
+  }
+
+  private static boolean isArrayWith(Tree tree, Predicate<Tree> predicate) {
+    if (tree instanceof ArrayExpression) {
+      ArrayExpression array = (ArrayExpression) tree;
       return array.elements().stream()
-        .anyMatch(IpRestrictedAdminAccessCheck::isSensitiveSourceAddress);
+        .anyMatch(predicate);
     }
     return false;
   }
