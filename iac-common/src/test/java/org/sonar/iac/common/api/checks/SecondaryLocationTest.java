29ee79cde57765fc2ed421afc1669b03f5efc9cc
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2023-06-20 09:41:04 +0200

    SONARIAC-790 Rebase and fix

--- iac-common/src/test/java/org/sonar/iac/common/api/checks/SecondaryLocationTest.java
+++ iac-common/src/test/java/org/sonar/iac/common/api/checks/SecondaryLocationTest.java
@@ -1,3 +1,22 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.iac.common.api.checks;
 
 import java.util.List;
@@ -9,7 +28,6 @@ import org.sonar.iac.common.checks.CommonTestUtils;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.sonar.iac.common.testing.IacCommonAssertions.assertThat;
 
-
 class SecondaryLocationTest {
 
   @Test
@@ -17,7 +35,7 @@ class SecondaryLocationTest {
     SecondaryLocation location = SecondaryLocation.secondary(2, 5, 8, 12, "message");
 
     assertThat(location.message).isEqualTo("message");
-    assertThat(location.textRange).hasRange(2,5,8,12);
+    assertThat(location.textRange).hasRange(2, 5, 8, 12);
   }
 
   @Test
@@ -26,7 +44,7 @@ class SecondaryLocationTest {
     SecondaryLocation location = SecondaryLocation.of(tree, "message");
 
     assertThat(location.message).isEqualTo("message");
-    assertThat(location.textRange).hasRange(1,0,1,5);
+    assertThat(location.textRange).hasRange(1, 0, 1, 5);
   }
 
   @Test
