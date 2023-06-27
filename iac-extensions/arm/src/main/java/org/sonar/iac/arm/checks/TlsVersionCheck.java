d9512075b171bae5057faf80ccfd318edd885640
Peter Trifanov <peter.trifanov@sonarsource.com>
2023-06-27 13:12:31 +0200

    Code style; fix typo in test resources

--- iac-extensions/arm/src/main/java/org/sonar/iac/arm/checks/TlsVersionCheck.java
+++ iac-extensions/arm/src/main/java/org/sonar/iac/arm/checks/TlsVersionCheck.java
@@ -1,3 +1,22 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.iac.arm.checks;
 
 import org.sonar.check.Rule;
@@ -13,7 +32,6 @@ public class TlsVersionCheck extends AbstractArmResourceCheck {
   private static final String STORAGE_ACCOUNT_TLS_PROPERTY_KEY = "minimumTlsVersion";
 
   private static final String DATABASE_TLS_PROPERTY_KEY = "minimalTlsVersion";
-
 
   @Override
   protected void registerResourceConsumer() {
