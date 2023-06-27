233e2659b87522570f00f89f90d517717b69f2e4
Peter Trifanov <peter.trifanov@sonarsource.com>
2023-06-27 14:30:17 +0200

    Review notes: refactoring and updating test resources

--- iac-extensions/arm/src/main/java/org/sonar/iac/arm/checks/TlsVersionCheck.java
+++ iac-extensions/arm/src/main/java/org/sonar/iac/arm/checks/TlsVersionCheck.java
@@ -23,6 +23,9 @@ import org.sonar.check.Rule;
 import org.sonar.iac.arm.checkdsl.ContextualResource;
 import org.sonar.iac.common.checks.TextUtils;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 @Rule(key = "S4423")
 public class TlsVersionCheck extends AbstractArmResourceCheck {
   private static final String TLS_VERSION_NOT_SET_MESSAGE = "Set minimumTlsVersion/minimalTlsVersion to disable support of older TLS versions.";
@@ -33,25 +36,21 @@ public class TlsVersionCheck extends AbstractArmResourceCheck {
 
   private static final String DATABASE_TLS_PROPERTY_KEY = "minimalTlsVersion";
 
+  private static final List<String> DATABASE_RESOURCE_TYPES = List.of(
+    "Microsoft.DBforMySQL/servers",
+    "Microsoft.DBforPostgreSQL/servers",
+    "Microsoft.DBforMariaDB/servers"
+  );
+
   @Override
   protected void registerResourceConsumer() {
-    register("Microsoft.Storage/storageAccounts", TlsVersionCheck::checkForStorageAccountTlsVersion);
-    register("Microsoft.DBforMySQL/servers", TlsVersionCheck::checkForDatabaseTlsVersion);
-    register("Microsoft.DBforPostgreSQL/servers", TlsVersionCheck::checkForDatabaseTlsVersion);
-    register("Microsoft.DBforMariaDB/servers", TlsVersionCheck::checkForDatabaseTlsVersion);
+    register("Microsoft.Storage/storageAccounts", checkTlsVersion(STORAGE_ACCOUNT_TLS_PROPERTY_KEY));
+    register(DATABASE_RESOURCE_TYPES, checkTlsVersion(DATABASE_TLS_PROPERTY_KEY));
   }
 
-  private static void checkForStorageAccountTlsVersion(ContextualResource resource) {
-    checkTlsVersion(STORAGE_ACCOUNT_TLS_PROPERTY_KEY, resource);
-  }
-
-  private static void checkForDatabaseTlsVersion(ContextualResource resource) {
-    checkTlsVersion(DATABASE_TLS_PROPERTY_KEY, resource);
-  }
-
-  private static void checkTlsVersion(String propertyName, ContextualResource resource) {
-    resource.property(propertyName)
+  private static Consumer<ContextualResource> checkTlsVersion(String propertyName) {
+    return resource -> resource.property(propertyName)
       .reportIfAbsent(TLS_VERSION_NOT_SET_MESSAGE)
-      .reportIf(e -> TextUtils.isValue(e, "TLS1_2").isFalse(), TLS_VERSION_INCORRECT_MESSAGE);
+      .reportIf(expr -> TextUtils.isValue(expr, "TLS1_2").isFalse(), TLS_VERSION_INCORRECT_MESSAGE);
   }
 }
