a1a7da321388ec821ea099ea6e6cba2f08bbf80a
Peter Trifanov <peter.trifanov@sonarsource.com>
2023-07-05 11:26:29 +0200

    Refactor `MeasuresTest.java` to use parameterized tests

--- its/plugin/src/test/java/org/sonarsource/iac/MeasuresTest.java
+++ its/plugin/src/test/java/org/sonarsource/iac/MeasuresTest.java
@@ -19,14 +19,36 @@
  */
 package org.sonarsource.iac;
 
-import com.sonar.orchestrator.build.SonarScanner;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MeasuresTest extends TestBase {
 
   private static final String BASE_DIRECTORY = "projects/measures/";
+
+  @ParameterizedTest
+  @CsvSource({
+    "cloudformationYamlMeasures, cloudformation, file1.yaml, 2, 5, 1, 1=1;4=1;5=1;6=1;7=1",
+    "cloudformationJsonMeasures, cloudformation, file1.json, 2, 9, 0, 1=1;2=1;3=1;4=1;5=1;6=1;7=1;8=1;9=1",
+    "kubernetesYamlMeasures, kubernetes, file_with_indicators.yml, 1, 10, 1, 1=1;2=1;3=1;4=1;5=1;7=1;8=1;9=1;10=1;11=1",
+    "dockerMeasures, docker, Dockerfile, 1, 7, 1, 2=1;3=1;4=1;5=1;6=1;7=1;8=1",
+  })
+  void testMeasures(String projectKey, String languageKey, String file, int expectedFiles, int expectedNcloc, int expectedCommentLines, String expectedNclocData) {
+    ORCHESTRATOR.executeBuild(getSonarScanner(projectKey, BASE_DIRECTORY, languageKey));
+
+    assertThat(getMeasureAsInt(projectKey, "files")).isEqualTo(expectedFiles);
+
+    final String fileKey = projectKey + ":" + file;
+    SoftAssertions softly = new SoftAssertions();
+    softly.assertThat(getMeasureAsInt(fileKey, "ncloc")).describedAs("ncloc").isEqualTo(expectedNcloc);
+    softly.assertThat(getMeasureAsInt(fileKey, "comment_lines")).describedAs("comment_lines").isEqualTo(expectedCommentLines);
+    softly.assertThat(getMeasure(fileKey, "ncloc_data").getValue()).describedAs("ncloc_data").isEqualTo(expectedNclocData);
+    softly.assertAll();
+  }
 
   @Test
   void terraform_measures() {
@@ -38,73 +60,17 @@ class MeasuresTest extends TestBase {
     final String emptyFile = projectKey + ":empty.tf";
     final String file1 = projectKey + ":file1.tf";
 
-    assertThat(getMeasure(emptyFile, "ncloc")).isNull();
-    assertThat(getMeasureAsInt(file1, "ncloc")).isEqualTo(14);
+    SoftAssertions softly = new SoftAssertions();
 
-    assertThat(getMeasure(emptyFile, "comment_lines")).isNull();
-    assertThat(getMeasureAsInt(file1, "comment_lines")).isEqualTo(5);
+    softly.assertThat(getMeasure(emptyFile, "ncloc")).isNull();
+    softly.assertThat(getMeasureAsInt(file1, "ncloc")).isEqualTo(14);
 
-    assertThat(getMeasure(emptyFile, "ncloc_data")).isNull();
-    assertThat(getMeasure(file1, "ncloc_data").getValue()).isEqualTo("3=1;4=1;5=1;7=1;8=1;9=1;10=1;11=1;14=1;22=1;23=1;24=1;25=1;26=1");
-  }
+    softly.assertThat(getMeasure(emptyFile, "comment_lines")).isNull();
+    softly.assertThat(getMeasureAsInt(file1, "comment_lines")).isEqualTo(5);
 
-  @Test
-  void cloudformation_yaml_measures() {
-    final String projectKey = "cloudformationYamlMeasures";
-    ORCHESTRATOR.executeBuild(getSonarScanner(projectKey, BASE_DIRECTORY, "cloudformation"));
+    softly.assertThat(getMeasure(emptyFile, "ncloc_data")).isNull();
+    softly.assertThat(getMeasure(file1, "ncloc_data").getValue()).isEqualTo("3=1;4=1;5=1;7=1;8=1;9=1;10=1;11=1;14=1;22=1;23=1;24=1;25=1;26=1");
 
-    assertThat(getMeasureAsInt(projectKey, "files")).isEqualTo(2);
-
-    final String file1 = projectKey + ":file1.yaml";
-
-    assertThat(getMeasureAsInt(file1, "ncloc")).isEqualTo(5);
-    assertThat(getMeasureAsInt(file1, "comment_lines")).isEqualTo(1);
-    assertThat(getMeasure(file1, "ncloc_data").getValue()).isEqualTo("1=1;4=1;5=1;6=1;7=1");
-  }
-
-  @Test
-  void cloudformation_json_measures() {
-    final String projectKey = "cloudformationJsonMeasures";
-    ORCHESTRATOR.executeBuild(getSonarScanner(projectKey, BASE_DIRECTORY, "cloudformation"));
-
-    assertThat(getMeasureAsInt(projectKey, "files")).isEqualTo(2);
-
-    final String file1 = projectKey + ":file1.json";
-
-    assertThat(getMeasureAsInt(file1, "ncloc")).isEqualTo(9);
-    assertThat(getMeasureAsInt(file1, "comment_lines")).isZero();
-    assertThat(getMeasure(file1, "ncloc_data").getValue()).isEqualTo("1=1;2=1;3=1;4=1;5=1;6=1;7=1;8=1;9=1");
-  }
-
-  @Test
-  void kubernetes_yaml_measures() {
-    final String projectKey = "kubernetesYamlMeasures";
-    SonarScanner scanner = getSonarScanner(projectKey, BASE_DIRECTORY, "kubernetes");
-
-    ORCHESTRATOR.executeBuild(scanner);
-
-    assertThat(getMeasureAsInt(projectKey, "files")).isEqualTo(1);
-
-    final String file1 = projectKey + ":file_with_indicators.yml";
-
-    assertThat(getMeasureAsInt(file1, "ncloc")).isEqualTo(10);
-    assertThat(getMeasureAsInt(file1, "comment_lines")).isEqualTo(1);
-    assertThat(getMeasure(file1, "ncloc_data").getValue()).isEqualTo("1=1;2=1;3=1;4=1;5=1;7=1;8=1;9=1;10=1;11=1");
-  }
-
-  @Test
-  void docker_measures() {
-    final String projectKey = "dockerMeasures";
-    SonarScanner scanner = getSonarScanner(projectKey, BASE_DIRECTORY, "docker");
-
-    ORCHESTRATOR.executeBuild(scanner);
-
-    assertThat(getMeasureAsInt(projectKey, "files")).isEqualTo(1);
-
-    final String file1 = projectKey + ":Dockerfile";
-
-    assertThat(getMeasureAsInt(file1, "ncloc")).isEqualTo(7);
-    assertThat(getMeasureAsInt(file1, "comment_lines")).isEqualTo(1);
-    assertThat(getMeasure(file1, "ncloc_data").getValue()).isEqualTo("2=1;3=1;4=1;5=1;6=1;7=1;8=1");
+    softly.assertAll();
   }
 }
