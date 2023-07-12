ec7e3ef690ad09c1777256e64f1b8af9f6f802a8
Peter Trifanov <peter.trifanov@sonarsource.com>
2023-07-12 13:26:58 +0200

    SONARIAC-932 Adapt targetScope Bicep implementation in Bicep FileImpl (#812)

--- iac-extensions/arm/src/test/java/org/sonar/iac/arm/tree/impl/bicep/FileImplTest.java
+++ iac-extensions/arm/src/test/java/org/sonar/iac/arm/tree/impl/bicep/FileImplTest.java
@@ -20,10 +20,12 @@
 package org.sonar.iac.arm.tree.impl.bicep;
 
 import org.junit.jupiter.api.Test;
-import org.sonar.iac.arm.ArmAssertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.sonar.iac.arm.parser.BicepParser;
 import org.sonar.iac.arm.parser.bicep.BicepLexicalGrammar;
 import org.sonar.iac.arm.tree.api.File;
+import org.sonar.iac.arm.tree.api.bicep.StringComplete;
 import org.sonar.iac.common.extension.ParseException;
 import org.sonar.iac.common.extension.visitors.InputFileContext;
 
@@ -60,5 +62,23 @@ class FileImplTest extends BicepTreeModelTest {
     assertThatThrownBy(() -> parser.parse(code, inputFile))
       .isInstanceOf(ParseException.class)
       .hasMessage("Cannot parse 'dir1/dir2/foo.bicep:1:1'");
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {
+    "'', RESOURCE_GROUP",
+    "targetScope = 'resourceGroup', RESOURCE_GROUP",
+    "targetScope = 'managementGroup', MANAGEMENT_GROUP",
+    "targetScope = 'subscription', SUBSCRIPTION",
+    "targetScope = 'tenant', TENANT",
+  })
+  void shouldDetectFileScope(String code, File.Scope expected) {
+    InputFileContext inputFile = createInputFileContextMock("foo.bicep");
+
+    BicepParser parser = BicepParser.create(BicepLexicalGrammar.FILE);
+    File tree = (File) parser.parse(code, inputFile);
+
+    assertThat(tree.targetScope()).isEqualTo(expected);
+    assertThat(tree.targetScopeLiteral()).satisfiesAnyOf(s -> assertThat(s).isInstanceOf(StringComplete.class), s -> assertThat(s).isNull());
   }
 }
