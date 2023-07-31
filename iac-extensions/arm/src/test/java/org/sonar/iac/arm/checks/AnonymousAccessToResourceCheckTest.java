99109279dc4db826a165164963f69cae0893d1ff
Peter Trifanov <peter.trifanov@sonarsource.com>
2023-07-31 12:29:15 +0200

    Static imports

--- iac-extensions/arm/src/test/java/org/sonar/iac/arm/checks/AnonymousAccessToResourceCheckTest.java
+++ iac-extensions/arm/src/test/java/org/sonar/iac/arm/checks/AnonymousAccessToResourceCheckTest.java
@@ -20,7 +20,8 @@
 package org.sonar.iac.arm.checks;
 
 import org.junit.jupiter.api.Test;
-import org.sonar.iac.common.testing.Verifier;
+
+import static org.sonar.iac.common.testing.Verifier.issue;
 
 class AnonymousAccessToResourceCheckTest {
   AnonymousAccessToResourceCheck check = new AnonymousAccessToResourceCheck();
@@ -28,35 +29,35 @@ class AnonymousAccessToResourceCheckTest {
   @Test
   void shouldFindIssuesInWebSitesResourceJson() {
     ArmVerifier.verify("AnonymousAccessToResourceCheck/Microsoft.Web_sites.json", check,
-      Verifier.issue(6, 14, 6, 35, "Omitting authsettingsV2 disables authentication. Make sure it is safe here."),
-      Verifier.issue(21, 14, 21, 44, "Make sure that disabling authentication is safe here."),
-      Verifier.issue(22, 14, 22, 61, "Make sure that disabling authentication is safe here."),
-      Verifier.issue(39, 14, 39, 44, "Make sure that disabling authentication is safe here."),
-      Verifier.issue(58, 14, 58, 61, "Make sure that disabling authentication is safe here."),
-      Verifier.issue(70, 10, 70, 40, "Make sure that disabling authentication is safe here."));
+      issue(6, 14, 6, 35, "Omitting authsettingsV2 disables authentication. Make sure it is safe here."),
+      issue(21, 14, 21, 44, "Make sure that disabling authentication is safe here."),
+      issue(22, 14, 22, 61, "Make sure that disabling authentication is safe here."),
+      issue(39, 14, 39, 44, "Make sure that disabling authentication is safe here."),
+      issue(58, 14, 58, 61, "Make sure that disabling authentication is safe here."),
+      issue(70, 10, 70, 40, "Make sure that disabling authentication is safe here."));
   }
 
   @Test
   void shouldFindIssuesInApiManagementServiceJson() {
     ArmVerifier.verify("AnonymousAccessToResourceCheck/Microsoft.ApiManagement_service.json", check,
-      Verifier.issue(6, 14, 6, 47, "Omitting sign_in authorizes anonymous access. Make sure it is safe here."),
-      Verifier.issue(20, 12, 20, 28, "Make sure that giving anonymous access without enforcing sign-in is safe here."),
-      Verifier.issue(26, 14, 26, 47, "Omitting sign_in authorizes anonymous access. Make sure it is safe here."),
-      Verifier.issue(31, 18, 31, 24, "Omitting authenticationSettings disables authentication. Make sure it is safe here."),
-      Verifier.issue(53, 18, 53, 24, "Omitting authenticationSettings disables authentication. Make sure it is safe here."),
-      Verifier.issue(69, 12, 69, 28, "Make sure that giving anonymous access without enforcing sign-in is safe here."),
-      Verifier.issue(88, 14, 88, 47, "Omitting sign_in authorizes anonymous access. Make sure it is safe here."));
+      issue(6, 14, 6, 47, "Omitting sign_in authorizes anonymous access. Make sure it is safe here."),
+      issue(20, 12, 20, 28, "Make sure that giving anonymous access without enforcing sign-in is safe here."),
+      issue(26, 14, 26, 47, "Omitting sign_in authorizes anonymous access. Make sure it is safe here."),
+      issue(31, 18, 31, 24, "Omitting authenticationSettings disables authentication. Make sure it is safe here."),
+      issue(53, 18, 53, 24, "Omitting authenticationSettings disables authentication. Make sure it is safe here."),
+      issue(69, 12, 69, 28, "Make sure that giving anonymous access without enforcing sign-in is safe here."),
+      issue(88, 14, 88, 47, "Omitting sign_in authorizes anonymous access. Make sure it is safe here."));
   }
 
   @Test
   void shouldFindIssuesInStorageAccountsJson() {
     ArmVerifier.verify("AnonymousAccessToResourceCheck/Microsoft.Storage_storageAccounts.json", check,
-      Verifier.issue(6, 14, 6, 49, "Make sure that authorizing potential anonymous access is safe here."),
-      Verifier.issue(15, 8, 15, 37, "Make sure that authorizing potential anonymous access is safe here."),
-      Verifier.issue(31, 12, 31, 34, "Make sure that authorizing potential anonymous access is safe here."),
-      Verifier.issue(37, 14, 37, 49, "Make sure that authorizing potential anonymous access is safe here."),
-      Verifier.issue(46, 12, 46, 34, "Make sure that authorizing potential anonymous access is safe here."),
-      Verifier.issue(56, 8, 56, 30, "Make sure that authorizing potential anonymous access is safe here."));
+      issue(6, 14, 6, 49, "Make sure that authorizing potential anonymous access is safe here."),
+      issue(15, 8, 15, 37, "Make sure that authorizing potential anonymous access is safe here."),
+      issue(31, 12, 31, 34, "Make sure that authorizing potential anonymous access is safe here."),
+      issue(37, 14, 37, 49, "Make sure that authorizing potential anonymous access is safe here."),
+      issue(46, 12, 46, 34, "Make sure that authorizing potential anonymous access is safe here."),
+      issue(56, 8, 56, 30, "Make sure that authorizing potential anonymous access is safe here."));
   }
 
   @Test
