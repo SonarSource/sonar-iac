65af4f5bb69eaac621888fd18e82b2154501f64c
Peter Trifanov <peter.trifanov@sonarsource.com>
2023-08-01 16:31:54 +0200

    License

--- iac-extensions/arm/src/test/java/org/sonar/iac/arm/checks/SecureValuesExposureCheckTest.java
+++ iac-extensions/arm/src/test/java/org/sonar/iac/arm/checks/SecureValuesExposureCheckTest.java
@@ -1,3 +1,22 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.iac.arm.checks;
 
 import org.junit.jupiter.api.Test;
@@ -14,11 +33,9 @@ class SecureValuesExposureCheckTest {
 
   @Test
   void testBicep() {
-    // ArmVerifier.verify("SecureValuesExposureCheck/Microsoft.Resources_deployments_compliant_secure_scope.json", new
-    // ArmVerifier.verify("SecureValuesExposureCheck/Microsoft.Resources_deployments_compliant_no_top_level_parameter.json", new
-    // SecureValuesExposureCheck());
+    BicepVerifier.verifyNoIssue("SecureValuesExposureCheck/Microsoft.Resources_deployments_compliant.bicep", new SecureValuesExposureCheck());
     BicepVerifier.verify("SecureValuesExposureCheck/Microsoft.Resources_deployments_noncompliant.bicep", new SecureValuesExposureCheck());
-    BicepVerifier.verify("SecureValuesExposureCheck/Microsoft.Resources_deployments_compliant.json", new SecureValuesExposureCheck());
+    BicepVerifier.verify("SecureValuesExposureCheck/Microsoft.Resources_deployments_compliant.bicep", new SecureValuesExposureCheck());
     // TODO: files `main.bicep` and `vm.bicep` are a preferred fix for the issue, but we don't support multiple files analysis yet
   }
 }
