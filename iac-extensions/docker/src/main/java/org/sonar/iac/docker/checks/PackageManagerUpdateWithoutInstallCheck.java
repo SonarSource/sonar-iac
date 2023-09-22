4a51f8f7589dff9e03c4bd4a79ce3bd27495df1b
Peter Trifanov <peter.trifanov@sonarsource.com>
2023-09-22 14:17:56 +0200

    SONARIAC-740 S6595 should not raise on commands with unresolved arguments

--- iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/PackageManagerUpdateWithoutInstallCheck.java
+++ iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/PackageManagerUpdateWithoutInstallCheck.java
@@ -38,7 +38,7 @@ public class PackageManagerUpdateWithoutInstallCheck implements IacCheck {
   private static final Set<String> PACKAGE_MANAGERS = Set.of("apk", "apt", "apt-get", "aptitude");
   private static final CommandDetector PACKAGE_MANAGER_DETECTOR = CommandDetector.builder()
     .with(PACKAGE_MANAGERS)
-    .withOptionalRepeating(s -> true)
+    .withAnyIncludingUnresolvedExcluding(s -> true)
     .build();
   private static final CommandDetector PACKAGE_MANAGER_UPDATE_DETECTOR = CommandDetector.builder()
     .with(PACKAGE_MANAGERS)
