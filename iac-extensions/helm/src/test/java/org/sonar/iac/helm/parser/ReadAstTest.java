cd152fd4c9a638a15f9b0d158d6711461eb909fa
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2023-11-16 11:58:48 +0100

    Position of nodes

--- iac-extensions/helm/src/test/java/org/sonar/iac/helm/parser/ReadAstTest.java
+++ iac-extensions/helm/src/test/java/org/sonar/iac/helm/parser/ReadAstTest.java
@@ -1,12 +1,13 @@
 package org.sonar.iac.helm.parser;
 
-import com.google.protobuf.Any;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.sonarsource.iac.helm.ActionNode;
-import org.sonarsource.iac.helm.GoTemplateAst;
+import org.sonarsource.iac.helm.FieldNode;
+import org.sonarsource.iac.helm.IfNode;
 import org.sonarsource.iac.helm.ListNode;
 import org.sonarsource.iac.helm.TextNode;
 
@@ -14,6 +15,7 @@ public class ReadAstTest {
   @Test
   void shouldReadL1M1() throws IOException {
     var listNode = ListNode.parseFrom(new FileInputStream("src/test/resources/L1M1.pb"));
+    var yamlFromFile = IOUtils.toString(new FileInputStream("src/test/resources/L1M1.yaml"), StandardCharsets.UTF_8);
     System.out.println(listNode);
 
     var node0 = listNode.getNodes(0);
@@ -23,6 +25,24 @@ public class ReadAstTest {
     // Throws ArrayIndexOutOfBoundsException: Index 4 out of bounds for length 4
     var actionNode = node1.unpack(ActionNode.class);
     var pos = actionNode.getPos();
-    System.out.println("Position: " + pos);
+    System.out.println("Action node Pos: " + pos);
+    System.out.println("Pos in YAML 167: " + yamlFromFile.substring(167, 200));
+
+    var fieldNode = actionNode.getPipe().getCmds(0).getArgs(0).unpack(FieldNode.class);
+    System.out.println("fieldNode: " + fieldNode);
+    System.out.println("fieldNode pos  : " + fieldNode.getPos());
+    System.out.println("Pos in YAML 174: " + yamlFromFile.substring(174, 210));
+
+  }
+
+  @Test
+  void shouldReadL1M5() throws IOException {
+    var listNode = ListNode.parseFrom(new FileInputStream("src/test/resources/L1M5.pb"));
+    var yamlFromFile = IOUtils.toString(new FileInputStream("src/test/resources/L1M1.yaml"), StandardCharsets.UTF_8);
+
+    var ifNode = listNode.getNodes(1).unpack(IfNode.class);
+    System.out.println("ifNode pos: 57");
+    System.out.println("Pos in YAML 57: " + yamlFromFile.substring(57, 100));
+
   }
 }
