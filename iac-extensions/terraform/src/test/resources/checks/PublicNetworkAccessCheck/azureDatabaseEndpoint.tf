d18fba254b96720ba3019619bcf6363dd81e05a9
Nils Werner <64034005+nils-werner-sonarsource@users.noreply.github.com>
2023-11-23 11:05:34 +0100

    SONARIAC-1142 S6329 should report only one issue for the same location (#963)

--- iac-extensions/terraform/src/test/resources/checks/PublicNetworkAccessCheck/azureDatabaseEndpoint.tf
+++ iac-extensions/terraform/src/test/resources/checks/PublicNetworkAccessCheck/azureDatabaseEndpoint.tf
@@ -29,6 +29,10 @@ resource "azurerm_application_insights" "production" {
   internet_query_enabled = false
 }
 
+# Noncompliant@+1 {{Omitting "internet_ingestion_enabled" and "internet_query_enabled" allows network access from the Internet. Make sure it is safe here.}}
+resource "azurerm_application_insights" "foo" {
+}
+
 #### sql_managed_instance ####
 
 resource "azurerm_sql_managed_instance" "production" {
