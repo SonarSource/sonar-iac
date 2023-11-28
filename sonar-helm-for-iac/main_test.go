b328c0d8dde16dfd2964882f94f43c7f3c571b9b
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2023-11-28 15:05:46 +0100

    Add Go test

--- sonar-helm-for-iac/main_test.go
+++ sonar-helm-for-iac/main_test.go
@@ -19,14 +19,12 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_evaluate_template(t *testing.T) {
-	fmt.Println("Hello World from test!")
 	template := `
 apiVersion: v1
 kind: Pod
@@ -46,6 +44,44 @@ spec:
 container:
   port: 8080
 `
+
+	expected := `
+apiVersion: v1
+kind: Pod
+metadata:
+  name: example
+spec:
+  containers:
+    - name: web
+      image: nginx
+      ports:
+        - name: web
+          containerPort: 8080
+          protocol: TCP
+`
+
+	result := evaluateTemplateInternal("a.yaml", template, values)
+
+	assert.Equal(t, result, expected)
+}
+
+func Test_evaluate_template_containing_sprig_functions(t *testing.T) {
+	template := `
+apiVersion: v1
+kind: {{ trim "  Pod  " }}
+metadata:
+  name: example
+spec:
+  containers:
+    - name: {{ lower "WEB" }}
+      image: {{ nospace "n g i  nx" }}
+      ports:
+        - name: {{ trunc 3 "website" }}
+          containerPort: {{ repeat 2 "80" }}
+          protocol: {{ upper "tcp" }}
+`
+
+	values := ""
 
 	expected := `
 apiVersion: v1
