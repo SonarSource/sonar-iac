096f0d400c4a5108fc21fa4d7cce172c822bc72a
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2024-01-04 16:08:28 +0100

    SONARIAC-1233 Create tests for the 'required' function in Helm template evaluation

---
+++ iac-extensions/kubernetes/src/test/java/org/sonar/iac/helm/methods/RequiredTest.java
@@ -0,0 +1,61 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.iac.helm.methods;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.sonar.api.impl.utils.DefaultTempFolder;
+import org.sonar.iac.helm.HelmEvaluator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+class RequiredTest {
+  @TempDir
+  static File tempDir;
+
+  private HelmEvaluator helmEvaluator;
+
+  @BeforeEach
+  void setUp() throws IOException {
+    this.helmEvaluator = new HelmEvaluator(new DefaultTempFolder(tempDir, false));
+    this.helmEvaluator.initialize();
+  }
+
+  @Test
+  void requireWithoutValueShouldThrowIllegalStateException() {
+    var templateDependencies = Map.of("values.yaml", "var1: myVal", "Chart.yaml", "name: foo");
+    Assertions
+      .assertThatThrownBy(() -> helmEvaluator.evaluateTemplate("/foo/bar/baz.yaml", "field: {{ required \"Missing mandatory var2 values!\" .Values.var2 }}", templateDependencies))
+      .isInstanceOf(IllegalStateException.class)
+      .hasMessageContaining("error calling required: Missing mandatory var2 values!");
+  }
+
+  @Test
+  void requireWithValueShouldBeProcessedNormally() throws IOException {
+    var templateDependencies = Map.of("values.yaml", "var1: myVal", "Chart.yaml", "name: foo");
+    var evaluationResult = helmEvaluator.evaluateTemplate("/foo/bar/baz.yaml", "field: {{ required \"Missing mandatory var2 values!\" .Values.var1 }}", templateDependencies);
+
+    Assertions.assertThat(evaluationResult.getTemplate()).contains("field: myVal");
+  }
+}
