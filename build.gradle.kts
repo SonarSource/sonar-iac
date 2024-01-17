fb99f7ba18fb23971c3c24c9040dcfda8456eb09
Peter Trifanov <peter.trifanov@sonarsource.com>
2024-01-17 16:22:10 +0100

    Experiment with setting projects' version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,6 @@ plugins {
     id("org.sonarsource.iac.sonarqube")
 }
 
-allprojects {
-    // this value is present on CI
-    val buildNumber: String? = System.getProperty("buildNumber")
-    project.ext["buildNumber"] = buildNumber
-    if (project.version.toString().endsWith("-SNAPSHOT") && buildNumber != null) {
-        val versionSuffix = if (project.version.toString().count { it == '.' } == 1) ".0.$buildNumber" else ".$buildNumber"
-        project.version = project.version.toString().replace("-SNAPSHOT", versionSuffix)
-        logger.lifecycle("Project version set to $version")
-    }
-}
-
 spotless {
     encoding(Charsets.UTF_8)
     kotlinGradle {
