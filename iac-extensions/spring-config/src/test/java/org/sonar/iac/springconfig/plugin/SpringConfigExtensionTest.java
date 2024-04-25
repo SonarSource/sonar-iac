6f2def45863c60ed4880167b7ac6e7ca989249d6
Peter Trifanov <peter.trifanov@sonarsource.com>
2024-04-25 16:44:08 +0200

    SONARIAC-1432 Add the `spring-config` subproject (#1271)

---
+++ iac-extensions/spring-config/src/test/java/org/sonar/iac/springconfig/plugin/SpringConfigExtensionTest.java
@@ -0,0 +1,40 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.iac.springconfig.plugin;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.api.Plugin;
+import org.sonar.api.SonarEdition;
+import org.sonar.api.SonarQubeSide;
+import org.sonar.api.internal.SonarRuntimeImpl;
+import org.sonar.api.utils.Version;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpringConfigExtensionTest {
+  @Test
+  void shouldRegisterExtensions() {
+    var runtime = SonarRuntimeImpl.forSonarQube(Version.create(10, 5), SonarQubeSide.SCANNER, SonarEdition.COMMUNITY);
+    var context = new Plugin.Context(runtime);
+    SpringConfigExtension.define(context);
+    assertThat(context.getExtensions()).isEmpty();
+  }
+
+}
