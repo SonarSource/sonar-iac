4e00b806784ed83c4ea9371b0377322d6b11118e
Jonas Wielage <jonas.wielage@sonarsource.com>
2024-05-03 14:25:37 +0200

    SONARIAC-1452 Set the correct profile name

--- iac-extensions/spring-config/src/test/java/org/sonar/iac/springconfig/parser/properties/PropertiesParseTreeVisitorTest.java
+++ iac-extensions/spring-config/src/test/java/org/sonar/iac/springconfig/parser/properties/PropertiesParseTreeVisitorTest.java
@@ -247,8 +247,11 @@ class PropertiesParseTreeVisitorTest {
     return Stream.of(
       arguments("spring.profiles.active=profile1", "profile1"),
       arguments("spring.config.activate.on-profile=dev & qa", "dev & qa"),
-      arguments("spring.profiles.active=profile1\nspring.config.activate.on-profile=profile2", "profile1 profile2"),
-      arguments("#comment", ""));
+      arguments("spring.profiles.active=profile1\nspring.config.activate.on-profile=profile2", "profile2"),
+      arguments("spring.profiles.active=profile1\nspring.profiles.active=profile2", "profile2"),
+      arguments("spring.profiles.default=profile1\nspring.profiles.active=profile2", "profile2"),
+      arguments("spring.profiles.default=profile1", "profile1"),
+      arguments("#comment", "default"));
   }
 
   @ParameterizedTest
@@ -274,7 +277,7 @@ class PropertiesParseTreeVisitorTest {
 
     var file = parseProperties(code);
 
-    assertThat(file.profiles().get(0).name()).isEmpty();
+    assertThat(file.profiles().get(0).name()).isEqualTo("default");
     assertThat(file.profiles().get(1).name()).isEqualTo(expectedProfileName);
   }
 
