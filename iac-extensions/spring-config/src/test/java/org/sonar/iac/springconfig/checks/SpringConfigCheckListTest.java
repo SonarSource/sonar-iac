63b703347f8ccc0768b45fc3288e66371ebe732b
Jonas Wielage <jonas.wielage@sonarsource.com>
2024-05-14 13:55:28 +0200

    Refactor comments and non performant string operations (#1314)

--- iac-extensions/spring-config/src/test/java/org/sonar/iac/springconfig/checks/SpringConfigCheckListTest.java
+++ iac-extensions/spring-config/src/test/java/org/sonar/iac/springconfig/checks/SpringConfigCheckListTest.java
@@ -20,7 +20,6 @@
 package org.sonar.iac.springconfig.checks;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import org.sonar.iac.common.testing.AbstractCheckListTest;
 
