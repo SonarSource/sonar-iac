aee515ccfaf78ef17b74d704c39ef25188b6582b
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2024-05-16 16:08:54 +0200

    Address review comment

--- iac-extensions/terraform/src/test/resources/checks/WeakSSLProtocolCheck/azure_mssql_server.tf
+++ iac-extensions/terraform/src/test/resources/checks/WeakSSLProtocolCheck/azure_mssql_server.tf
@@ -1,23 +1,23 @@
-resource "azurerm_mssql_server" "mysql_noncompliant_old" {
+resource "azurerm_mssql_server" "mssql_noncompliant_old" {
   minimum_tls_version = "1.0"  # Noncompliant {{Change this code to disable support of older TLS versions.}}
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
 
-resource "azurerm_mssql_server" "mysql_noncompliant_old" {
+resource "azurerm_mssql_server" "mssql_noncompliant_old" {
   minimum_tls_version = "Disabled"  # Noncompliant
 }
 
-resource "azurerm_mssql_server" "mysql_noncompliant_old" {
+resource "azurerm_mssql_server" "mssql_noncompliant_old" {
   minimum_tls_version = "invalid value"  # Noncompliant
 }
 
-resource "azurerm_mssql_server" "mysql_compliant_missing" {
+resource "azurerm_mssql_server" "mssql_compliant_missing" {
 }
 
-resource "azurerm_mssql_server" "mysql_compliant" {
+resource "azurerm_mssql_server" "mssql_compliant" {
   minimum_tls_version  = "1.2"
 }
 
-resource "not_azurerm_mssql_server" "mysql_coverage" {
+resource "not_azurerm_mssql_server" "mssql_coverage" {
   minimum_tls_version  = "TLS1_0"
 }
