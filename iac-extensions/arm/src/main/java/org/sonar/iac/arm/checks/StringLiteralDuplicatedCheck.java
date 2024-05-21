d53c2ca5edc54eb77f6ed311521f5513acc196eb
Peter Trifanov <peter.trifanov@sonarsource.com>
2024-05-21 11:23:01 +0200

    Review notes

--- iac-extensions/arm/src/main/java/org/sonar/iac/arm/checks/StringLiteralDuplicatedCheck.java
+++ iac-extensions/arm/src/main/java/org/sonar/iac/arm/checks/StringLiteralDuplicatedCheck.java
@@ -48,6 +48,11 @@ public class StringLiteralDuplicatedCheck implements IacCheck {
   private static final String SECONDARY_MESSAGE = "Duplication.";
   private static final Pattern ALLOWED_DUPLICATED_LITERALS = Pattern.compile("(?U)^[\\p{L}_][.\\-\\w]+$");
   private static final Pattern ALLOWED_VERSION_NUMBER = Pattern.compile("^\\d++[.-]\\d++[.-]\\d++[.-]*\\d*+$");
+  /**
+   * ARM has a <a href=https://learn.microsoft.com/en-us/azure/azure-resource-manager/templates/template-functions-string#format><code>format</code></a> function
+   * that supports the same format specifiers as <code>System.String.Format</code> in .NET.
+   * These are described <a href=https://learn.microsoft.com/en-us/dotnet/fundamentals/runtime-libraries/system-string-format#get-started-with-the-stringformat-method>here</a>.
+   */
   protected static final Pattern FORMAT_STRING = Pattern.compile("(\\{\\d++(,-?\\d*)?(:[^}]+)?})++");
 
   public static final int THRESHOLD_DEFAULT = 5;
