6071f384b08bd16f6a5e01ed35acb5f9b0f7d2c5
Peter Trifanov <peter.trifanov@sonarsource.com>
2024-05-23 15:47:30 +0200

    SONARIAC-1022 S6506 Detection should not be thwarted by addition of parameters (#1346)

--- iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/ClearTextProtocolDowngradeCheck.java
+++ iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/ClearTextProtocolDowngradeCheck.java
@@ -19,6 +19,7 @@
  */
 package org.sonar.iac.docker.checks;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -42,34 +43,42 @@ public class ClearTextProtocolDowngradeCheck implements IacCheck {
   private static final String WGET_MESSAGE = "Not disabling redirects might allow for redirections to insecure websites. Make sure it is safe here.";
   private static final String CURL_COMMAND = "curl";
   private static final String PROTO_FLAG = "--proto";
+  private static final Set<String> WGET_NO_REDIRECT_FLAGS = Set.of("--max-redirect=0", "--max-redirect='0'", "--max-redirect=\"0\"");
   private static final Set<String> REDIRECTION_FLAGS = Set.of("-L", "--location");
   private static final Set<String> SENSITIVE_FLAGS = Set.of("-L", "--location", PROTO_FLAG);
+  // Curl supports "short options immediately next to each other", like -SfsL, we need special detection for this.
+  private static final String REDIRECTION_SHORT_FLAG = "L";
   private static final Predicate<String> EQUALS_PROTO_FLAG_OPTION = equalsIgnoreQuotes("=https");
   private static final Predicate<String> SENSITIVE_HTTPS_URL_BEGINNING = startsWithIgnoreQuotes("https");
-  private static final Predicate<String> OPTIONAL_OTHER_FLAGS = s -> s.startsWith("-") && !SENSITIVE_FLAGS.contains(s);
+
+  private static final Predicate<String> SHORT_FLAG = s -> s.startsWith("-") && (s.length() == 1 || s.charAt(1) != '-');
+  private static final Predicate<String> LONG_FLAG = s -> s.startsWith("--");
+  private static final Predicate<String> LONG_OTHER_FLAG = LONG_FLAG.and(not(longFlagContainingOneOf(SENSITIVE_FLAGS)));
+  private static final Predicate<String> SHORT_OTHER_FLAG = SHORT_FLAG.and(not(shortFlagContaining(REDIRECTION_SHORT_FLAG)));
+  private static final Predicate<String> OPTIONAL_OTHER_FLAGS = LONG_OTHER_FLAG.or(SHORT_OTHER_FLAG);
 
   // common predicates of detectors
   private static final CommandDetector.Builder REDIRECTION_PREDICATES = CommandDetector.builder()
-    .withOptional(OPTIONAL_OTHER_FLAGS)
-    .with(REDIRECTION_FLAGS)
-    .withOptional(OPTIONAL_OTHER_FLAGS);
+    .withOptionalRepeating(OPTIONAL_OTHER_FLAGS)
+    .with(longFlagContainingOneOf(REDIRECTION_FLAGS).or(shortFlagContaining(REDIRECTION_SHORT_FLAG)))
+    .withOptionalRepeating(OPTIONAL_OTHER_FLAGS);
 
   private static final CommandDetector.Builder PROTO_FLAG_MISSING_OPTION_PREDICATES = CommandDetector.builder()
-    .withOptional(OPTIONAL_OTHER_FLAGS)
+    .withOptionalRepeating(OPTIONAL_OTHER_FLAGS)
     .with(PROTO_FLAG)
     .notWith(EQUALS_PROTO_FLAG_OPTION)
-    .withOptional(OPTIONAL_OTHER_FLAGS);
+    .withOptionalRepeating(OPTIONAL_OTHER_FLAGS);
 
   private static final CommandDetector.Builder PROTO_FLAG_MISSING_PREDICATES = CommandDetector.builder()
-    .withOptional(OPTIONAL_OTHER_FLAGS)
+    .withOptionalRepeating(OPTIONAL_OTHER_FLAGS)
     .notWith(PROTO_FLAG::equals)
-    .withOptional(OPTIONAL_OTHER_FLAGS);
+    .withOptionalRepeating(OPTIONAL_OTHER_FLAGS);
 
   private static final CommandDetector.Builder PROTO_FLAG_WITH_WRONG_OPTION_PREDICATES = CommandDetector.builder()
-    .withOptional(OPTIONAL_OTHER_FLAGS)
+    .withOptionalRepeating(OPTIONAL_OTHER_FLAGS)
     .with(PROTO_FLAG)
     .with(not(EQUALS_PROTO_FLAG_OPTION))
-    .withOptional(OPTIONAL_OTHER_FLAGS);
+    .withOptionalRepeating(OPTIONAL_OTHER_FLAGS);
 
   // actual detectors
   // matching "curl -L --proto https://redirecttoinsecure.example.com"
@@ -112,10 +121,12 @@ public class ClearTextProtocolDowngradeCheck implements IacCheck {
     .with(SENSITIVE_HTTPS_URL_BEGINNING)
     .build();
 
+  // For wget, arguments can be both before and after the URL, unless there is `--` before the URL.
   private static final CommandDetector WGET_DETECTOR = CommandDetector.builder()
     .with("wget")
+    .withAnyFlagExcept(WGET_NO_REDIRECT_FLAGS)
     .with(SENSITIVE_HTTPS_URL_BEGINNING)
-    .withAnyFlagExcept("--max-redirect=0")
+    .withAnyFlagExcept(WGET_NO_REDIRECT_FLAGS)
     .build();
 
   private static final Set<CommandDetector> SENSITIVE_CURL_COMMAND_DETECTORS = Set.of(
@@ -136,5 +147,13 @@ public class ClearTextProtocolDowngradeCheck implements IacCheck {
     SENSITIVE_CURL_COMMAND_DETECTORS.forEach(
       commandDetector -> commandDetector.search(resolvedArgument).forEach(command -> ctx.reportIssue(command, CURL_MESSAGE)));
     WGET_DETECTOR.search(resolvedArgument).forEach(command -> ctx.reportIssue(command, WGET_MESSAGE));
+  }
+
+  private static Predicate<String> longFlagContainingOneOf(Collection<String> flags) {
+    return LONG_FLAG.and(flags::contains);
+  }
+
+  private static Predicate<String> shortFlagContaining(String flag) {
+    return SHORT_FLAG.and(s -> s.contains(flag));
   }
 }
--- iac-extensions/docker/src/test/resources/checks/ClearTextProtocolDowngradeCheck/curl.dockerfile
+++ iac-extensions/docker/src/test/resources/checks/ClearTextProtocolDowngradeCheck/curl.dockerfile
@@ -3,48 +3,83 @@ FROM ubuntu:22.04
 # Noncompliant@+1 {{Not enforcing HTTPS here might allow for redirections to insecure websites. Make sure it is safe here.}}
 RUN curl -L https://redirecttoinsecure.example.com
 #   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
 # Noncompliant@+1
 RUN curl --location https://redirecttoinsecure.example.com
 
-
 # Noncompliant@+1
 RUN curl -L --proto "=foo" https://redirecttoinsecure.example.com
+# Noncompliant@+1
+RUN curl --location --proto "=foo" https://redirecttoinsecure.example.com
 
 # Noncompliant@+1
 RUN curl --proto "=foo" -L https://redirecttoinsecure.example.com
+# Noncompliant@+1
+RUN curl --proto "=foo" --location https://redirecttoinsecure.example.com
 
 # Noncompliant@+1
 RUN curl -L --proto https://redirecttoinsecure.example.com
+# Noncompliant@+1
+RUN curl --location --proto https://redirecttoinsecure.example.com
 
 # Noncompliant@+1
 RUN curl --proto -L https://redirecttoinsecure.example.com
+# Noncompliant@+1
+RUN curl --proto --location https://redirecttoinsecure.example.com
 
 # Noncompliant@+1
 RUN curl -L --proto "=foobar" https://redirecttoinsecure.example.com
+# Noncompliant@+1
+RUN curl --location --proto "=foobar" https://redirecttoinsecure.example.com
 
 # Noncompliant@+1
 RUN curl --proto "=foobar" -L https://redirecttoinsecure.example.com
+# Noncompliant@+1
+RUN curl --proto "=foobar" --location https://redirecttoinsecure.example.com
 
 # Noncompliant@+1
 RUN curl -foo -L -foo --proto "=foobar" https://redirecttoinsecure.example.com
+# Noncompliant@+1
+RUN curl -foo --location -foo --proto "=foobar" https://redirecttoinsecure.example.com
+
 
 # Noncompliant@+1
 RUN curl --proto "=foobar" -foo -L -foo https://redirecttoinsecure.example.com
+# Noncompliant@+1
+RUN curl --proto "=foobar" -foo --location -foo https://redirecttoinsecure.example.com
+
+# Noncompliant@+1
+RUN curl --tlsv1.2 -sSf -L https://might-redirect.example.com/install.sh
+# Noncompliant@+1
+RUN curl --tlsv1.2 -sSf --location https://might-redirect.example.com/install.sh
+
+# Noncompliant@+1
+RUN curl --tlsv1.2 -sSfL https://might-redirect.example.com/install.sh
+# Noncompliant@+1
+RUN curl -sSfL https://might-redirect.example.com/install.sh
+# Noncompliant@+1
+RUN curl -sSfL --tlsv1.2 https://might-redirect.example.com/install.sh
+# Noncompliant@+1
+RUN curl -LsSf --tlsv1.2 https://might-redirect.example.com/install.sh
+# Noncompliant@+1
+RUN curl -sLSf --tlsv1.2 https://might-redirect.example.com/install.sh
 
 RUN curl -L --proto "=foobar" http://redirecttoinsecure.example.com
+RUN curl --location --proto "=foobar" http://redirecttoinsecure.example.com
 
 RUN curl --proto -L "=foobar" http://redirecttoinsecure.example.com
+RUN curl --proto --location "=foobar" http://redirecttoinsecure.example.com
 
 RUN curl --proto "=https" -L https://redirecttoinsecure.example.com
+RUN curl --proto "=https" --location https://redirecttoinsecure.example.com
 
 RUN curl --proto '=https' -L https://redirecttoinsecure.example.com
-
-RUN curl --location http://redirecttoinsecure.example.com
+RUN curl --proto '=https' --location https://redirecttoinsecure.example.com
 
 RUN curl -L http://redirecttoinsecure.example.com
+RUN curl --location http://redirecttoinsecure.example.com
 
 RUN curl --proto "=https" -L https://redirecttoinsecure.example.com | true
+RUN curl --proto "=https" --location https://redirecttoinsecure.example.com | true
 
 RUN curl https://redirecttoinsecure.example.com
 
