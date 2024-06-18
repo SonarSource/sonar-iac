6a8c8b7902a7bca6f905deed1da854c52ea91654
Peter Trifanov <peter.trifanov@sonarsource.com>
2024-06-18 11:13:37 +0200

    SONARIAC-1507 Convert AST resources to "ProjectResource" classes (#1363)

---
+++ iac-extensions/kubernetes/src/main/java/org/sonar/iac/kubernetes/visitors/ProjectResourceFactory.java
@@ -0,0 +1,112 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.iac.kubernetes.visitors;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.annotation.CheckForNull;
+import org.sonar.iac.common.api.tree.HasTextRange;
+import org.sonar.iac.common.checks.PropertyUtils;
+import org.sonar.iac.common.checks.TextUtils;
+import org.sonar.iac.common.checks.Trilean;
+import org.sonar.iac.common.yaml.tree.MappingTree;
+import org.sonar.iac.common.yaml.tree.ScalarTree;
+import org.sonar.iac.common.yaml.tree.SequenceTree;
+import org.sonar.iac.common.yaml.tree.TupleTree;
+import org.sonar.iac.common.yaml.tree.YamlTree;
+import org.sonar.iac.kubernetes.model.LimitRange;
+import org.sonar.iac.kubernetes.model.LimitRangeItem;
+import org.sonar.iac.kubernetes.model.ProjectResource;
+import org.sonar.iac.kubernetes.model.ServiceAccount;
+
+public final class ProjectResourceFactory {
+  private ProjectResourceFactory() {
+    // utility class
+  }
+
+  @CheckForNull
+  public static ProjectResource createServiceAccount(MappingTree tree) {
+    if (!PropertyUtils.valueIs(tree, "kind", kind -> TextUtils.isValue(kind, "ServiceAccount").isTrue())) {
+      return null;
+    }
+
+    var name = PropertyUtils.value(tree, "metadata", MappingTree.class)
+      .flatMap(metadata -> PropertyUtils.value(metadata, "name"))
+      .map(ScalarTree.class::cast)
+      .map(ScalarTree::value);
+    if (name.isEmpty()) {
+      return null;
+    }
+
+    var automountServiceAccountTokenTree = PropertyUtils.value(tree, "automountServiceAccountToken")
+      .map(ScalarTree.class::cast);
+    var automountServiceAccountToken = automountServiceAccountTokenTree
+      .map(TextUtils::trileanFromTextTree)
+      .orElse(Trilean.UNKNOWN);
+    var valueLocation = automountServiceAccountTokenTree.map(HasTextRange::textRange).orElse(null);
+
+    return new ServiceAccount(name.get(), automountServiceAccountToken, valueLocation);
+  }
+
+  @CheckForNull
+  public static ProjectResource createLimitRange(MappingTree tree) {
+    if (!PropertyUtils.valueIs(tree, "kind", kind -> TextUtils.isValue(kind, "LimitRange").isTrue())) {
+      return null;
+    }
+
+    var limits = PropertyUtils.value(tree, "spec")
+      .flatMap(it -> PropertyUtils.value(it, "limits"))
+      .stream()
+      .flatMap(it -> ((SequenceTree) it).elements().stream())
+      .filter(MappingTree.class::isInstance)
+      .map(MappingTree.class::cast)
+      .map(ProjectResourceFactory::toLimitRangeItem)
+      .toList();
+
+    return new LimitRange(limits);
+  }
+
+  private static LimitRangeItem toLimitRangeItem(MappingTree tree) {
+    var map = tree.elements().stream()
+      .collect(Collectors.toMap(tuple -> ((ScalarTree) tuple.key()).value(), TupleTree::value));
+
+    return new LimitRangeItem(
+      ((ScalarTree) map.get("type")).value(),
+      mappingTreeToMap("default", map),
+      mappingTreeToMap("defaultRequest", map),
+      mappingTreeToMap("maxLimitRequestRatio", map),
+      mappingTreeToMap("max", map),
+      mappingTreeToMap("min", map));
+  }
+
+  private static Map<String, String> mappingTreeToMap(String key, Map<String, YamlTree> values) {
+    if (!values.containsKey(key)) {
+      return Map.of();
+    }
+
+    var tree = values.get(key);
+    if (tree instanceof MappingTree mappingTree) {
+      return mappingTree.elements().stream()
+        .collect(Collectors.toMap(tuple -> ((ScalarTree) tuple.key()).value(), tuple -> ((ScalarTree) tuple.value()).value()));
+    } else {
+      return Map.of();
+    }
+  }
+}
---
+++ iac-extensions/kubernetes/src/test/java/org/sonar/iac/kubernetes/visitors/ProjectResourceFactoryTest.java
@@ -0,0 +1,245 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.iac.kubernetes.visitors;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.sonar.iac.common.checks.Trilean;
+import org.sonar.iac.common.extension.TreeParser;
+import org.sonar.iac.common.yaml.tree.FileTree;
+import org.sonar.iac.common.yaml.tree.MappingTree;
+import org.sonar.iac.kubernetes.model.LimitRange;
+import org.sonar.iac.kubernetes.model.ServiceAccount;
+import org.sonar.iac.kubernetes.plugin.HelmProcessor;
+import org.sonar.iac.kubernetes.plugin.KubernetesParser;
+import org.sonar.iac.kubernetes.plugin.KubernetesParserStatistics;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class ProjectResourceFactoryTest {
+  private static final TreeParser<FileTree> PARSER = new KubernetesParser(
+    mock(HelmProcessor.class),
+    new KubernetesParserStatistics());
+
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void shouldCreateServiceAccount(boolean automountServiceAccountToken) {
+    var code = """
+      apiVersion: v1
+      kind: ServiceAccount
+      metadata:
+        name: my-service-account
+        namespace: my-namespace
+      automountServiceAccountToken: %s
+      """.formatted(automountServiceAccountToken);
+    var tree = (MappingTree) PARSER.parse(code, null).documents().get(0);
+
+    var serviceAccount = (ServiceAccount) ProjectResourceFactory.createServiceAccount(tree);
+
+    assertThat(serviceAccount).isNotNull();
+    assertThat(serviceAccount.name()).isEqualTo("my-service-account");
+    if (automountServiceAccountToken) {
+      assertThat(serviceAccount.automountServiceAccountToken()).isEqualTo(Trilean.TRUE);
+    } else {
+      assertThat(serviceAccount.automountServiceAccountToken()).isEqualTo(Trilean.FALSE);
+    }
+  }
+
+  @Test
+  void shouldCreateServiceAccountWithoutAutomountServiceAccountToken() {
+    // language=yaml
+    var code = """
+      apiVersion: v1
+      kind: ServiceAccount
+      metadata:
+        name: my-service-account
+        namespace: my-namespace
+      """;
+    var tree = (MappingTree) PARSER.parse(code, null).documents().get(0);
+
+    var serviceAccount = ProjectResourceFactory.createServiceAccount(tree);
+
+    assertThat(serviceAccount).isNotNull()
+      .extracting("name", "automountServiceAccountToken")
+      .containsExactly("my-service-account", Trilean.UNKNOWN);
+  }
+
+  @Test
+  void shouldReturnNullIfServiceAccountHasNoName() {
+    // language=yaml
+    var code = """
+      apiVersion: v1
+      kind: ServiceAccount
+      metadata:
+        namespace: my-namespace
+      """;
+    var tree = (MappingTree) PARSER.parse(code, null).documents().get(0);
+
+    var serviceAccount = ProjectResourceFactory.createServiceAccount(tree);
+
+    assertThat(serviceAccount).isNull();
+  }
+
+  @Test
+  void shouldCreateMemoryLimit() {
+    // language=yaml
+    var code = """
+      apiVersion: v1
+      kind: LimitRange
+      metadata:
+        name: name constraint
+      spec:
+        limits:
+        - default:
+            cpu: 0.5
+          defaultRequest:
+            memory: 50Mi\s
+          maxLimitRequestRatio:
+            cpu: 500m
+          max:
+            storage: 2Gi
+          min:
+            storage: 1Gi
+          type: Container
+      """;
+    var tree = (MappingTree) PARSER.parse(code, null).documents().get(0);
+
+    var memoryLimit = (LimitRange) ProjectResourceFactory.createLimitRange(tree);
+
+    assertThat(memoryLimit).isNotNull();
+    assertThat(memoryLimit.limits()).hasSize(1);
+
+    var limit = memoryLimit.limits().get(0);
+    assertThat(limit.defaultMap()).containsEntry("cpu", "0.5");
+    assertThat(limit.defaultRequestMap()).containsEntry("memory", "50Mi");
+    assertThat(limit.maxLimitRequestRatioMap()).containsEntry("cpu", "500m");
+    assertThat(limit.max()).containsEntry("storage", "2Gi");
+    assertThat(limit.min()).containsEntry("storage", "1Gi");
+  }
+
+  @Test
+  void shouldCreateLimitRangeWithoutSomeLimits() {
+    // language=yaml
+    var code = """
+      apiVersion: v1
+      kind: LimitRange
+      metadata:
+        name: name constraint
+      spec:
+        limits:
+        - default:
+            cpu: 0.5
+          type: Container
+      """;
+    var tree = (MappingTree) PARSER.parse(code, null).documents().get(0);
+
+    var memoryLimit = (LimitRange) ProjectResourceFactory.createLimitRange(tree);
+
+    var limit = memoryLimit.limits().get(0);
+    assertThat(limit.defaultMap()).containsEntry("cpu", "0.5");
+    assertThat(limit.defaultRequestMap()).isEmpty();
+    assertThat(limit.maxLimitRequestRatioMap()).isEmpty();
+    assertThat(limit.max()).isEmpty();
+    assertThat(limit.min()).isEmpty();
+  }
+
+  @Test
+  void shouldCreateLimitRangeFromMultipleLimits() {
+    // language=yaml
+    var code = """
+      apiVersion: v1
+      kind: LimitRange
+      metadata:
+        name: name constraint
+      spec:
+        limits:
+        - default:
+            cpu: 0.5
+          type: Container
+        - defaultRequest:
+            memory: 50Mi
+          type: Pod
+      """;
+    var tree = (MappingTree) PARSER.parse(code, null).documents().get(0);
+
+    var memoryLimit = (LimitRange) ProjectResourceFactory.createLimitRange(tree);
+
+    assertThat(memoryLimit.limits()).hasSize(2);
+
+    var containerLimit = memoryLimit.limits().get(0);
+    assertThat(containerLimit.defaultMap()).containsEntry("cpu", "0.5");
+    assertThat(containerLimit.defaultRequestMap()).isEmpty();
+    assertThat(containerLimit.maxLimitRequestRatioMap()).isEmpty();
+    assertThat(containerLimit.max()).isEmpty();
+    assertThat(containerLimit.min()).isEmpty();
+
+    var podLimit = memoryLimit.limits().get(1);
+    assertThat(podLimit.defaultMap()).isEmpty();
+    assertThat(podLimit.defaultRequestMap()).containsEntry("memory", "50Mi");
+    assertThat(podLimit.maxLimitRequestRatioMap()).isEmpty();
+    assertThat(podLimit.max()).isEmpty();
+    assertThat(podLimit.min()).isEmpty();
+  }
+
+  @Test
+  void shouldPutEmptyMapIfLimitItemIsNotMapping() {
+    // language=yaml
+    var code = """
+      apiVersion: v1
+      kind: LimitRange
+      metadata:
+        name: name constraint
+      spec:
+        limits:
+        - default: 0.5
+          type: Container
+      """;
+    var tree = (MappingTree) PARSER.parse(code, null).documents().get(0);
+
+    var limitRange = (LimitRange) ProjectResourceFactory.createLimitRange(tree);
+
+    var limit = limitRange.limits().get(0);
+    assertThat(limit.defaultMap()).isEmpty();
+  }
+
+  @Test
+  void shouldReturnNullForOtherKinds() {
+    // language=yaml
+    var code = """
+      apiVersion: v1
+      kind: Pod
+      metadata:
+        name: my-pod
+      spec:
+        containers:
+        - name: my-container
+          image: my-image
+      """;
+    var tree = (MappingTree) PARSER.parse(code, null).documents().get(0);
+
+    var serviceAccount = ProjectResourceFactory.createServiceAccount(tree);
+    assertThat(serviceAccount).isNull();
+
+    var limitRange = ProjectResourceFactory.createLimitRange(tree);
+    assertThat(limitRange).isNull();
+  }
+}
