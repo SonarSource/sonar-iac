58d0c7e7e3fef4ac1f0bed2adc50a6a489bed245
Peter Trifanov <peter.trifanov@sonarsource.com>
2024-07-03 16:42:16 +0200

    SONARIAC-1293 S117: Local variable and method parameter names should comply with a naming convention

--- iac-extensions/kubernetes/src/main/java/org/sonar/iac/kubernetes/checks/KubernetesCheckList.java
+++ iac-extensions/kubernetes/src/main/java/org/sonar/iac/kubernetes/checks/KubernetesCheckList.java
@@ -44,6 +44,7 @@ public final class KubernetesCheckList {
       MemoryLimitCheck.class,
       MemoryRequestCheck.class,
       MountingFileSystemPathsCheck.class,
+      NameConventionCheck.class,
       ParsingErrorCheck.class,
       PrivilegeEscalationCheck.class,
       RBACWildcardCheck.class,
