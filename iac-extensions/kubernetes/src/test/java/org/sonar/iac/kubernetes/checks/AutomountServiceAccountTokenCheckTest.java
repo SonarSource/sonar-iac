9826fb32a973bd534a1032605f3cd808403e9613
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2024-07-11 11:36:49 +0200

    Address review comment

--- iac-extensions/kubernetes/src/test/java/org/sonar/iac/kubernetes/checks/AutomountServiceAccountTokenCheckTest.java
+++ iac-extensions/kubernetes/src/test/java/org/sonar/iac/kubernetes/checks/AutomountServiceAccountTokenCheckTest.java
@@ -81,8 +81,7 @@ class AutomountServiceAccountTokenCheckTest {
   void shouldRaiseSecondaryLocationOnLinkedAccount() {
     String root = "AutomountServiceAccountTokenCheck/LinkedAccount/NonCompliant/SensitiveValue/";
     var secondaryLocation1 = new SecondaryLocation(TextRanges.range(10, 22, 10, 45), "Through this service account");
-    var secondaryLocation2 = new SecondaryLocation(TextRanges.range(6, 30, 6, 34), "Change this setting",
-      "AutomountServiceAccountTokenCheck/LinkedAccount/NonCompliant/SensitiveValue/linked_account_service_token.yaml");
+    var secondaryLocation2 = new SecondaryLocation(TextRanges.range(6, 30, 6, 34), "Change this setting", root + "linked_account_service_token.yaml");
     Verifier.Issue expectedIssue = new Verifier.Issue(TextRanges.range(6, 0, 6, 4), "Set automountServiceAccountToken to false for this specification of kind Pod.",
       List.of(secondaryLocation1, secondaryLocation2));
     KubernetesVerifier.verify(root + "automount_service_account_token_pod_linked.yaml", check, List.of(expectedIssue), root + "linked_account_service_token.yaml");
