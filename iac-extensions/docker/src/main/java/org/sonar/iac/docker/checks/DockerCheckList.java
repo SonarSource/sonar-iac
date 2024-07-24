bd63c67b3bcb3d1efa0f13d02fba419be2a34501
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2024-07-24 09:26:08 +0200

    SONARIAC-1540 S7021: WORKDIR instruction should only be used with absolute path (#1430)

--- iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/DockerCheckList.java
+++ iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/DockerCheckList.java
@@ -66,6 +66,7 @@ public final class DockerCheckList {
       VariableReferenceOutsideOfQuotesCheck.class,
       WeakHashAlgorithmsCheck.class,
       WeakSslTlsProtocolsCheck.class,
+      WorkdirAbsolutPathCheck.class,
       WorkdirInsteadCdCheck.class);
   }
 }
