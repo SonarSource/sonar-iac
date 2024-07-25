c4db282a22d1f282a023498f88c791a7ea50f768
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2024-07-25 10:26:03 +0200

    SONARIAC-1555 S7031: Reduce the amount of consecutive RUN instruction

---
+++ iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/ConsecutiveRunInstructionCheck.java
@@ -0,0 +1,71 @@
+/*
+ * SonarQube IaC Plugin
+ * Copyright (C) 2021-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.iac.docker.checks;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.iac.common.api.checks.CheckContext;
+import org.sonar.iac.common.api.checks.IacCheck;
+import org.sonar.iac.common.api.checks.InitContext;
+import org.sonar.iac.common.api.checks.SecondaryLocation;
+import org.sonar.iac.docker.tree.api.DockerImage;
+import org.sonar.iac.docker.tree.api.DockerTree;
+import org.sonar.iac.docker.tree.api.RunInstruction;
+
+@Rule(key = "S7031")
+public class ConsecutiveRunInstructionCheck implements IacCheck {
+  private static final String PRIMARY_MESSAGE = "Merge this RUN instruction with the consecutive ones.";
+  private static final String SECONDARY_MESSAGE = "consecutive RUN instruction";
+
+  @Override
+  public void initialize(InitContext init) {
+    init.register(DockerImage.class, ConsecutiveRunInstructionCheck::checkFromRunsInstruction);
+  }
+
+  private static void checkFromRunsInstruction(CheckContext ctx, DockerImage image) {
+    var listOfConsecutiveRunInstructions = extractConsecutiveRunInstructions(image);
+    for (List<RunInstruction> consecutiveRunInstructions : listOfConsecutiveRunInstructions) {
+      var secondaryLocations = consecutiveRunInstructions.stream()
+        .skip(1)
+        .map(runInstruction -> new SecondaryLocation(runInstruction.keyword(), SECONDARY_MESSAGE))
+        .toList();
+      ctx.reportIssue(consecutiveRunInstructions.get(0).keyword(), PRIMARY_MESSAGE, secondaryLocations);
+    }
+  }
+
+  private static List<List<RunInstruction>> extractConsecutiveRunInstructions(DockerImage image) {
+    var result = new ArrayList<List<RunInstruction>>();
+    var instructions = image.instructions();
+    for (var i = 0; i < instructions.size() - 1; i++) {
+      if (instructions.get(i).is(DockerTree.Kind.RUN)) {
+        var subList = new ArrayList<RunInstruction>();
+        while (i < instructions.size() && instructions.get(i).is(DockerTree.Kind.RUN)) {
+          subList.add((RunInstruction) instructions.get(i));
+          i++;
+        }
+        if (subList.size() > 1) {
+          result.add(subList);
+        }
+      }
+    }
+    return result;
+  }
+}
