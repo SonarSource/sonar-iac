bf86371663ce184441544acf82dc43e3549aefc6
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2024-07-25 14:20:34 +0200

    SONARIAC-1551 Docker parser should support instruction CROSS_BUILD_COPY (#1438)

--- iac-extensions/docker/src/main/java/org/sonar/iac/docker/parser/DockerPreprocessor.java
+++ iac-extensions/docker/src/main/java/org/sonar/iac/docker/parser/DockerPreprocessor.java
@@ -20,7 +20,10 @@
 package org.sonar.iac.docker.parser;
 
 import com.sonar.sslr.api.typed.Input;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
@@ -31,12 +34,14 @@ import org.sonar.iac.common.api.tree.Comment;
 import org.sonar.iac.common.api.tree.impl.CommentImpl;
 import org.sonar.iac.common.api.tree.impl.TextRange;
 import org.sonar.iac.common.api.tree.impl.TextRanges;
+import org.sonar.iac.docker.parser.grammar.DockerKeyword;
 
 import static org.sonar.iac.common.parser.grammar.LexicalConstant.WHITESPACE;
 import static org.sonar.iac.docker.parser.grammar.DockerLexicalConstant.EOL;
 
 public class DockerPreprocessor {
 
+  private static final Map<String, Pattern> PATTERN_CACHE = new HashMap<>();
   private static final String NOT_EOL_CHARS = "[^\\n\\r\\u2028\\u2029]";
   private static final String COMMENT = "(#" + NOT_EOL_CHARS + "*+)";
   private static final String INLINE_COMMENT_OR_EMPTY_LINE = "(?<!" + NOT_EOL_CHARS + ")(?<inlineCommentOrEmptyLine>(?:[" + WHITESPACE + "]*+" + COMMENT + "?(?:" + EOL + "|$))*)";
@@ -48,11 +53,22 @@ public class DockerPreprocessor {
   private static final Pattern ALTERNATIVE_ESCAPE_CHAR_PATTERN = Pattern.compile("^(#[^" + EOL + "]*+" + EOL + "|\\s)*" + ALTERNATIVE_ESCAPE_CHAR_DIRECTIVE);
   private static final Pattern COMMENT_PATTERN = Pattern.compile(COMMENT);
 
+  // Extract list of docker instructions from DockerKeyword
+  private static final List<String> DOCKER_INSTRUCTIONS = Arrays.stream(DockerKeyword.values()).map(DockerKeyword::getValue).toList();
+  /**
+   * Regex to remove prefix {@code CROSS_BUILD_} according to strict rules to avoid FP as much as possible:
+   * - it must prefix a valid dockerfile instruction
+   * - it must be at the beginning of a line/file
+   * - the full instruction must be followed by a whitespace
+   */
+  private static final String CROSS_BUILD_REGEX = "(?<=^|[\r\n])CROSS_BUILD_(?=(?:" + String.join("|", DOCKER_INSTRUCTIONS) + ")[" + WHITESPACE + "])";
+
   private static final Set<String> COMMENT_TYPES = Set.of("commentLine", "inlineCommentOrEmptyLine");
 
   /**
    * Remove every escaped line break. This results in instructions being represented in one line at a time.
    * Track removed characters to adjust the offset when creating syntax tokens.
+   * Also remove prefix on instructions starting with {@code CROSS_BUILD_}. This is an unofficial feature that is widely used.
    */
   public PreprocessorResult process(String source) {
     Input input = new Input(source.toCharArray());
@@ -101,13 +117,16 @@ public class DockerPreprocessor {
 
   private static Matcher matchRemovableSequences(String source) {
     String escapeCharacter = determineEscapeCharacter(source);
+    return PATTERN_CACHE.computeIfAbsent(escapeCharacter, DockerPreprocessor::computePattern).matcher(source);
+  }
 
+  private static Pattern computePattern(String escapeCharacter) {
     String escapedLineBreaks = "(?<escapedLineBreaks>(?<!escape=)" + escapeCharacter + "[" + WHITESPACE + "]*+" + EOL + ")";
     String multiLineInstruction = escapedLineBreaks + INLINE_COMMENT_OR_EMPTY_LINE;
 
-    String pattern = "(?:" + multiLineInstruction + "|" + COMMENT_LINE + ")";
+    String pattern = "(?:" + multiLineInstruction + "|" + COMMENT_LINE + "|" + CROSS_BUILD_REGEX + ")";
 
-    return Pattern.compile(pattern).matcher(source);
+    return Pattern.compile(pattern);
   }
 
   static String determineEscapeCharacter(String source) {
