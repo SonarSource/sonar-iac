73752d344bda0624be524b0ae6564c9e6beb314c
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2024-08-23 15:35:51 +0200

    Remove unused imports

--- iac-common/src/test/java/org/sonar/iac/common/reports/ExternalReportWildcardProviderTest.java
+++ iac-common/src/test/java/org/sonar/iac/common/reports/ExternalReportWildcardProviderTest.java
@@ -25,7 +25,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
