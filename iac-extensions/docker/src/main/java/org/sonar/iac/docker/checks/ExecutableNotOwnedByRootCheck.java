b916e541c926b6f4383200c20dcacac19d860c51
Peter Trifanov <peter.trifanov@sonarsource.com>
2024-09-06 14:45:28 +0200

    SONARIAC-1295 S6504 should raise an issue independently from the file extension (#1510)

--- iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/ExecutableNotOwnedByRootCheck.java
+++ iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/ExecutableNotOwnedByRootCheck.java
@@ -19,23 +19,21 @@
  */
 package org.sonar.iac.docker.checks;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import javax.annotation.CheckForNull;
 import org.sonar.check.Rule;
 import org.sonar.iac.common.api.checks.CheckContext;
 import org.sonar.iac.common.api.checks.IacCheck;
 import org.sonar.iac.common.api.checks.InitContext;
 import org.sonar.iac.common.api.checks.SecondaryLocation;
 import org.sonar.iac.common.api.tree.HasTextRange;
-import org.sonar.iac.docker.checks.utils.CheckUtils;
 import org.sonar.iac.docker.checks.utils.Chmod;
 import org.sonar.iac.docker.symbols.ArgumentResolution;
 import org.sonar.iac.docker.tree.api.Argument;
 import org.sonar.iac.docker.tree.api.Flag;
 import org.sonar.iac.docker.tree.api.TransferInstruction;
-
-import javax.annotation.CheckForNull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 @Rule(key = "S6504")
 public class ExecutableNotOwnedByRootCheck implements IacCheck {
@@ -43,8 +41,6 @@ public class ExecutableNotOwnedByRootCheck implements IacCheck {
   private static final String MESSAGE = "Make sure no write permissions are assigned to the executable.";
   private static final String MESSAGE_SECONDARY_OTHER_EXEC = "Other executable file.";
   private static final String MESSAGE_SECONDARY_CHOWN = "Sensitive file owner.";
-  private static final Set<String> SENSITIVE_FILE_EXTENSION = Set.of("exe", "py", "rb", "pl", "lua", "js", "lisp", "sh", "jar", "war",
-    "run", "bin", "bat", "ps1");
 
   private static final Set<String> COMPLIANT_CHOWN_VALUES = Set.of("root", "0", "");
 
@@ -57,7 +53,7 @@ public class ExecutableNotOwnedByRootCheck implements IacCheck {
     Flag sensitiveChownFlag = getSensitiveChownFlag(transferInstruction);
 
     if (sensitiveChownFlag != null) {
-      List<Argument> sensitiveFiles = getSensitiveFiles(transferInstruction.srcs());
+      List<Argument> sensitiveFiles = transferInstruction.srcs();
 
       if (isNonRootUser(sensitiveChownFlag)) {
         reportIssue(ctx, sensitiveChownFlag, sensitiveFiles);
@@ -117,18 +113,6 @@ public class ExecutableNotOwnedByRootCheck implements IacCheck {
       .map(argResolved -> new Chmod(null, null, argResolved.value()))
       .findFirst()
       .orElse(null);
-  }
-
-  private static List<Argument> getSensitiveFiles(List<Argument> arguments) {
-    return arguments.stream()
-      .map(ArgumentResolution::of)
-      .filter(ExecutableNotOwnedByRootCheck::isSensitiveFile)
-      .map(ArgumentResolution::argument)
-      .toList();
-  }
-
-  private static boolean isSensitiveFile(ArgumentResolution argumentResolution) {
-    return argumentResolution.isResolved() && SENSITIVE_FILE_EXTENSION.contains(CheckUtils.getFileExtension(argumentResolution.value()));
   }
 
   private static boolean isSensitiveWriteChmod(Chmod chmod) {
