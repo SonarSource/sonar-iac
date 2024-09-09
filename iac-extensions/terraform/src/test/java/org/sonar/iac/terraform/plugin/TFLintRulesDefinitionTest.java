322d6dd3303594fa46ee4fd6ccf2c76aaa238164
Jonas Wielage <jonas.wielage@sonarsource.com>
2024-09-09 12:10:08 +0200

    Increase coverage

--- iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/plugin/TFLintRulesDefinitionTest.java
+++ iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/plugin/TFLintRulesDefinitionTest.java
@@ -19,14 +19,23 @@
  */
 package org.sonar.iac.terraform.plugin;
 
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.sonar.api.SonarProduct;
 import org.sonar.api.SonarRuntime;
+import org.sonar.api.issue.impact.Severity;
+import org.sonar.api.issue.impact.SoftwareQuality;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.iac.common.reports.AbstractExternalRulesDefinition;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.sonar.api.rules.CleanCodeAttribute.CONVENTIONAL;
+import static org.sonar.api.rules.CleanCodeAttribute.LOGICAL;
+import static org.sonar.api.rules.RuleType.BUG;
+import static org.sonar.api.rules.RuleType.CODE_SMELL;
+import static org.sonar.api.rules.RuleType.SECURITY_HOTSPOT;
 import static org.sonar.iac.common.testing.AbstractExternalRulesDefinitionAssertions.assertExistingRepository;
 import static org.sonar.iac.common.testing.AbstractExternalRulesDefinitionAssertions.assertNoRepositoryIsDefined;
 import static org.sonar.iac.common.testing.IacTestUtils.SONAR_QUBE_10_6_CCT_SUPPORT_MINIMAL_VERSION;
@@ -58,9 +67,41 @@ class TFLintRulesDefinitionTest {
   @Test
   void noOpRulesDefinitionShouldNotDefineAnyRule() {
     var context = new RulesDefinition.Context();
-    AbstractExternalRulesDefinition noOpRulesDefinition =
-      TFLintRulesDefinition.noOpInstanceForSL(SONAR_QUBE_10_6_CCT_SUPPORT_MINIMAL_VERSION);
+    AbstractExternalRulesDefinition noOpRulesDefinition = TFLintRulesDefinition.noOpInstanceForSL(SONAR_QUBE_10_6_CCT_SUPPORT_MINIMAL_VERSION);
     noOpRulesDefinition.define(context);
     assertNoRepositoryIsDefined(context, noOpRulesDefinition);
+  }
+
+  @Test
+  void externalRulesShouldBeLoadedCorrectly() {
+    var context = new RulesDefinition.Context();
+    AbstractExternalRulesDefinition rulesDefinition = new TFLintRulesDefinition(SONAR_QUBE_10_6_CCT_SUPPORT_MINIMAL_VERSION);
+    rulesDefinition.define(context);
+    var repository = context.repository("external_tflint");
+
+    RulesDefinition.Rule ruleCodeSmell = repository.rule("terraform_comment_syntax");
+    assertThat(ruleCodeSmell).isNotNull();
+    assertThat(ruleCodeSmell.name()).isEqualTo("Terraform comment syntax");
+    assertThat(ruleCodeSmell.tags()).isEmpty();
+    assertThat(ruleCodeSmell.type()).isEqualTo(CODE_SMELL);
+    assertThat(ruleCodeSmell.cleanCodeAttribute()).isEqualTo(CONVENTIONAL);
+    assertThat(ruleCodeSmell.defaultImpacts()).containsOnly(Map.entry(SoftwareQuality.MAINTAINABILITY, Severity.MEDIUM));
+
+    RulesDefinition.Rule ruleBug = repository.rule("azurerm_linux_virtual_machine_invalid_size");
+    assertThat(ruleBug).isNotNull();
+    assertThat(ruleBug.name()).isEqualTo("Azurerm linux virtual machine invalid size");
+    assertThat(ruleBug.tags()).isEmpty();
+    assertThat(ruleBug.type()).isEqualTo(BUG);
+    assertThat(ruleBug.cleanCodeAttribute()).isEqualTo(LOGICAL);
+    assertThat(ruleBug.defaultImpacts()).containsOnly(Map.entry(SoftwareQuality.RELIABILITY, Severity.MEDIUM));
+
+    RulesDefinition.Rule ruleSecurityHotspot = repository.rule("terraform_module_pinned_source");
+    assertThat(ruleSecurityHotspot).isNotNull();
+    assertThat(ruleSecurityHotspot.name()).isEqualTo("Terraform module pinned source");
+    assertThat(ruleSecurityHotspot.tags()).isEmpty();
+    assertThat(ruleSecurityHotspot.type()).isEqualTo(SECURITY_HOTSPOT);
+    // Security Hotspots do not have a clean code attribute or impacts
+    assertThat(ruleSecurityHotspot.cleanCodeAttribute()).isNull();
+    assertThat(ruleSecurityHotspot.defaultImpacts()).isEmpty();
   }
 }
