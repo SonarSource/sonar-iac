2ff100dccff54f986f007bc95dd8590da8f2282a
Jonas Wielage <jonas.wielage@sonarsource.com>
2024-09-09 16:35:30 +0200

    SONARIAC-1590 Update Tflint rule metadata with new Software Qualities (#1490)

--- iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/plugin/TFLintRulesDefinitionTest.java
+++ iac-extensions/terraform/src/test/java/org/sonar/iac/terraform/plugin/TFLintRulesDefinitionTest.java
@@ -19,14 +19,25 @@
  */
 package org.sonar.iac.terraform.plugin;
 
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.sonar.api.SonarProduct;
 import org.sonar.api.SonarRuntime;
+import org.sonar.api.issue.impact.Severity;
+import org.sonar.api.issue.impact.SoftwareQuality;
+import org.sonar.api.rules.CleanCodeAttribute;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.iac.common.reports.AbstractExternalRulesDefinition;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.sonar.api.rules.CleanCodeAttribute.CONVENTIONAL;
+import static org.sonar.api.rules.CleanCodeAttribute.LOGICAL;
+import static org.sonar.api.rules.RuleType.BUG;
+import static org.sonar.api.rules.RuleType.CODE_SMELL;
+import static org.sonar.api.rules.RuleType.SECURITY_HOTSPOT;
+import static org.sonar.api.rules.RuleType.VULNERABILITY;
 import static org.sonar.iac.common.testing.AbstractExternalRulesDefinitionAssertions.assertExistingRepository;
 import static org.sonar.iac.common.testing.AbstractExternalRulesDefinitionAssertions.assertNoRepositoryIsDefined;
 import static org.sonar.iac.common.testing.IacTestUtils.SONAR_QUBE_10_6_CCT_SUPPORT_MINIMAL_VERSION;
@@ -60,5 +71,33 @@ class TFLintRulesDefinitionTest {
     AbstractExternalRulesDefinition noOpRulesDefinition = TFLintRulesDefinition.noOpInstanceForSL(SONAR_QUBE_10_6_CCT_SUPPORT_MINIMAL_VERSION);
     noOpRulesDefinition.define(context);
     assertNoRepositoryIsDefined(context, noOpRulesDefinition);
+  }
+
+  @Test
+  void externalRulesShouldCarryTheCorrectAttributeAndImpact() {
+    var context = new RulesDefinition.Context();
+    AbstractExternalRulesDefinition rulesDefinition = new TFLintRulesDefinition(SONAR_QUBE_10_6_CCT_SUPPORT_MINIMAL_VERSION);
+    rulesDefinition.define(context);
+    var repository = context.repository("external_tflint");
+
+    for (String ruleKey : rulesDefinition.getRuleLoader().ruleKeys()) {
+      var rule = repository.rule(ruleKey);
+      var ruleType = rule.type();
+      CleanCodeAttribute cleanCodeAttribute = rule.cleanCodeAttribute();
+      Map<SoftwareQuality, Severity> impacts = rule.defaultImpacts();
+      if (ruleType == CODE_SMELL) {
+        assertThat(cleanCodeAttribute).isEqualTo(CONVENTIONAL);
+        assertThat(impacts).containsOnly(Map.entry(SoftwareQuality.MAINTAINABILITY, Severity.MEDIUM));
+      } else if (ruleType == BUG) {
+        assertThat(cleanCodeAttribute).isEqualTo(LOGICAL);
+        assertThat(impacts).containsOnly(Map.entry(SoftwareQuality.RELIABILITY, Severity.MEDIUM));
+      } else if (ruleType == SECURITY_HOTSPOT) {
+        // Security Hotspots do not have a clean code attribute or impacts
+        assertThat(cleanCodeAttribute).isNull();
+        assertThat(impacts).isEmpty();
+      } else if (ruleType == VULNERABILITY) {
+        throw new IllegalStateException("No rule should have type VULNERABILITY");
+      }
+    }
   }
 }
