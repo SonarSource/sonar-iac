8ded3ce570661f18afeb0b543f8b7b81153d4641
Rudy Regazzoni <110470341+rudy-regazzoni-sonarsource@users.noreply.github.com>
2024-12-18 16:32:03 +0100

    SONARIAC-1873 Add documentation so that method "setShouldReportSecondaryInValues" is not called for security hotspots

--- iac-extensions/kubernetes/src/common/java/org/sonar/iac/kubernetes/visitors/KubernetesCheckContext.java
+++ iac-extensions/kubernetes/src/common/java/org/sonar/iac/kubernetes/visitors/KubernetesCheckContext.java
@@ -23,6 +23,13 @@ import org.sonar.iac.common.extension.visitors.InputFileContext;
 public interface KubernetesCheckContext extends CheckContext {
   boolean shouldReportSecondaryInValues();
 
+  /**
+   * In case a rule is reporting an issue on a data that originate from a values.yaml file, calling this method and passing
+   * {@code true} will allow to automatically add a secondary location to the related values in the values.yaml file.
+   * This should not be enabled for rules that are security hotspots, because current SonarQube UI is not displaying them properly. They are
+   * showed at the correct text range location but in the primary file, which is incorrect and misleading.
+   * @param shouldReport {@code true} to enable secondary locations in values.yaml, {@code false} otherwise.
+   */
   void setShouldReportSecondaryInValues(boolean shouldReport);
 
   void reportIssueNoLineShift(TextRange toHighlight, String message);
