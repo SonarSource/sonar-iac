cd280e3ea8eca937769f1114138251fc06335f98
Peter Trifanov <peter.trifanov@sonarsource.com>
2025-11-03 09:59:20 +0100

    SONARIAC-2230 S6596 should have different message when tag is present but not compliant (#584)
    GitOrigin-RevId: 08e2ed5ed69a6492a28ef72040137fa5361540ee

--- iac-extensions/kubernetes/src/common/java/org/sonar/iac/kubernetes/checks/SpecificVersionTagCheck.java
+++ iac-extensions/kubernetes/src/common/java/org/sonar/iac/kubernetes/checks/SpecificVersionTagCheck.java
@@ -17,20 +17,30 @@
 package org.sonar.iac.kubernetes.checks;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
+import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.iac.common.checks.TextUtils;
 import org.sonar.iac.common.yaml.object.BlockObject;
+import org.sonar.iac.common.yaml.tree.TupleTree;
 import org.sonar.iac.common.yaml.tree.YamlTree;
+import org.sonar.iac.kubernetes.visitors.KubernetesCheckContext;
 
 @Rule(key = "S6596")
 public class SpecificVersionTagCheck extends AbstractKubernetesObjectCheck {
   public static final String MESSAGE = "Use a specific version tag for the image.";
+  private static final String MESSAGE_SPECIFIC_FORMAT = "Use a specific version tag for the image instead of \"%s\".";
   protected static final String KIND_POD = "Pod";
   protected static final List<String> KIND_WITH_TEMPLATE = List.of("DaemonSet", "Deployment", "Job", "ReplicaSet", "ReplicationController", "StatefulSet", "CronJob");
   private static final Predicate<YamlTree> SENSITIVE_VERSION_TAG_PREDICATE = tree -> TextUtils.matchesValue(tree, SpecificVersionTagCheck::hasSensitiveVersionTag)
     .isTrue();
+
+  @Override
+  void initializeCheck(KubernetesCheckContext ctx) {
+    ctx.setShouldReportSecondaryInValues(true);
+  }
 
   @Override
   protected void registerObjectCheck() {
@@ -48,7 +58,12 @@ public class SpecificVersionTagCheck extends AbstractKubernetesObjectCheck {
     }
     containers
       .map(container -> container.attribute("image"))
-      .forEach(image -> image.reportIfValue(sensitiveVersionTagPredicate(), MESSAGE));
+      .filter(image -> image.isValue(sensitiveVersionTagPredicate()))
+      .forEach(image -> image.reportOnValue(Optional.ofNullable(image.tree)
+        .map(TupleTree::value)
+        .map(SpecificVersionTagCheck::extractTag)
+        .map(MESSAGE_SPECIFIC_FORMAT::formatted)
+        .orElse(MESSAGE)));
   }
 
   public Predicate<YamlTree> sensitiveVersionTagPredicate() {
@@ -56,16 +71,37 @@ public class SpecificVersionTagCheck extends AbstractKubernetesObjectCheck {
   }
 
   public static boolean hasSensitiveVersionTag(String fullImageName) {
-    if (fullImageName.isBlank() || fullImageName.contains("@")) {
-      // image name is empty, unresolved with Helm, or using digest: do not raise an issue
+    var tagInfo = parseImageReference(fullImageName);
+    if (!tagInfo.shouldProcess) {
       return false;
     }
-    if (fullImageName.contains(":")) {
-      // raise an issue if the version tag is "latest"
-      String[] splitImageName = fullImageName.split(":");
-      return splitImageName.length > 1 && "latest".equals(splitImageName[1]);
+    // Flag if explicit "latest" or no tag specified (implicit latest)
+    return tagInfo.tag == null || "latest".equals(tagInfo.tag);
+  }
+
+  @Nullable
+  private static String extractTag(YamlTree tree) {
+    var imageValue = TextUtils.getValue(tree).orElse("");
+    var tagInfo = parseImageReference(imageValue);
+    return tagInfo.shouldProcess ? tagInfo.tag : null;
+  }
+
+  private static ImageTagInfo parseImageReference(String imageReference) {
+    // image name is empty, unresolved with Helm, or using digest: do not raise an issue
+    if (imageReference.isBlank() || imageReference.contains("@") || imageReference.startsWith("$")) {
+      return new ImageTagInfo(false, null);
     }
+
+    if (imageReference.contains(":")) {
+      var parts = imageReference.split(":", 2);
+      String tag = parts.length > 1 ? parts[1] : null;
+      return new ImageTagInfo(true, tag);
+    }
+
     // no version tag specified, kubernetes assumes "latest"
-    return !fullImageName.startsWith("$");
+    return new ImageTagInfo(true, null);
+  }
+
+  private record ImageTagInfo(boolean shouldProcess, @Nullable String tag) {
   }
 }
