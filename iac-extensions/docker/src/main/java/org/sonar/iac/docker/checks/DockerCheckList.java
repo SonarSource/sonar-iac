0e2c0a0066e5803e3a79106bbc601ad70452ac79
Jonas Wielage <jonas.wielage@sonarsource.com>
2025-11-26 14:16:46 +0100

    SONARIAC-2480 Migrate S6597 from Docker community to Docker EE and replace CommandDetector usage with equivalent ShellCmdDetectorBuilder
    GitOrigin-RevId: 20dad1ea97d9174969a732a60dfb9012950c063f

--- iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/DockerCheckList.java
+++ iac-extensions/docker/src/main/java/org/sonar/iac/docker/checks/DockerCheckList.java
@@ -67,7 +67,6 @@ public final class DockerCheckList {
       UnsecureConnectionCheck.class,
       WeakHashAlgorithmsCheck.class,
       WeakSslTlsProtocolsCheck.class,
-      WorkdirAbsolutPathCheck.class,
-      WorkdirInsteadCdCheck.class);
+      WorkdirAbsolutPathCheck.class);
   }
 }
