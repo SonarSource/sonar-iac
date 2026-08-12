412cb072d371f95795fc60d9f23f6e3983295de0
Teemu R. <teemu.rytilahti@sonarsource.com>
2026-08-12 17:54:39 +0200

    SONARIAC-3099 S6403: Honor ssl_mode over deprecated require_ssl (#1156)
    google_sql_database_instance's ip_configuration.ssl_mode now overrides require_ssl entirely when set, matching GCP's deprecation of require_ssl in favor of ssl_mode.
    
    GitOrigin-RevId: 27c72fef42cb851bd440aec499fa69275088f4f1

--- iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/gcp/DatabaseIpConfigCheck.java
+++ iac-extensions/terraform/src/main/java/org/sonar/iac/terraform/checks/gcp/DatabaseIpConfigCheck.java
@@ -18,24 +18,40 @@ package org.sonar.iac.terraform.checks.gcp;
 
 import org.sonar.check.Rule;
 import org.sonar.iac.terraform.checks.AbstractNewResourceCheck;
+import org.sonar.iac.terraform.symbols.BlockSymbol;
 
 import static org.sonar.iac.terraform.checks.utils.ExpressionPredicate.isFalse;
 
 @Rule(key = "S6403")
 public class DatabaseIpConfigCheck extends AbstractNewResourceCheck {
 
-  private static final String MESSAGE = "Make sure creating a GCP SQL instance without requiring TLS is safe here.";
-  private static final String OMITTING_MESSAGE = "Omitting \"%s\" allows unencrypted connections to the database. Make sure it is safe here.";
+  private static final String MESSAGE = "Change to disallow unencrypted connections.";
+  private static final String OMITTING_MESSAGE = "Omitting \"%s\" allows unencrypted connections to the database.";
+  private static final String ALLOW_UNENCRYPTED_AND_ENCRYPTED = "ALLOW_UNENCRYPTED_AND_ENCRYPTED";
 
   @Override
   protected void registerResourceConsumer() {
     register("google_sql_database_instance",
       resource -> resource.block("settings")
-        .reportIfAbsent(OMITTING_MESSAGE)
+        .reportIfAbsent(String.format(OMITTING_MESSAGE, "settings.ip_configuration.ssl_mode"))
         .block("ip_configuration")
-        .reportIfAbsent(OMITTING_MESSAGE)
-        .attribute("require_ssl")
-        .reportIfAbsent(OMITTING_MESSAGE)
-        .reportIf(isFalse(), MESSAGE));
+        .reportIfAbsent(String.format(OMITTING_MESSAGE, "ip_configuration.ssl_mode"))
+        .consume(DatabaseIpConfigCheck::checkIpConfiguration));
+  }
+
+  private static void checkIpConfiguration(BlockSymbol ipConfiguration) {
+    var sslMode = ipConfiguration.attribute("ssl_mode");
+    var sslModeValue = sslMode.asString();
+    if (sslModeValue != null) {
+      // require_ssl is deprecated in favor of ssl_mode, which takes precedence over require_ssl when both are set:
+      // https://docs.cloud.google.com/sql/docs/postgres/admin-api/rest/v1/instances#ipconfiguration
+      if (ALLOW_UNENCRYPTED_AND_ENCRYPTED.equals(sslModeValue)) {
+        sslMode.report(MESSAGE);
+      }
+      return;
+    }
+    ipConfiguration.attribute("require_ssl")
+      .reportIfAbsent(String.format(OMITTING_MESSAGE, "ssl_mode"))
+      .reportIf(isFalse(), MESSAGE);
   }
 }
--- iac-extensions/terraform/src/test/resources/checks/DatabaseIpConfigCheck/gcp_sql_database_instance.tf
+++ iac-extensions/terraform/src/test/resources/checks/DatabaseIpConfigCheck/gcp_sql_database_instance.tf
@@ -1,17 +1,17 @@
-# Noncompliant@+1 {{Omitting "settings" allows unencrypted connections to the database. Make sure it is safe here.}}
+# Noncompliant@+1 {{Omitting "settings.ip_configuration.ssl_mode" allows unencrypted connections to the database.}}
 resource "google_sql_database_instance" "noncompliant1" {
   #      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
 
 resource "google_sql_database_instance" "noncompliant2" {
-  settings { # Noncompliant {{Omitting "ip_configuration" allows unencrypted connections to the database. Make sure it is safe here.}}
+  settings { # Noncompliant {{Omitting "ip_configuration.ssl_mode" allows unencrypted connections to the database.}}
 # ^^^^^^^^
   }
 }
 
 resource "google_sql_database_instance" "noncompliant3" {
   settings {
-    ip_configuration { # Noncompliant {{Omitting "require_ssl" allows unencrypted connections to the database. Make sure it is safe here.}}
+    ip_configuration { # Noncompliant {{Omitting "ssl_mode" allows unencrypted connections to the database.}}
   # ^^^^^^^^^^^^^^^^
     }
   }
@@ -21,7 +21,7 @@ resource "google_sql_database_instance" "noncompliant3" {
 resource "google_sql_database_instance" "noncompliant4" {
   settings {
     ip_configuration {
-      require_ssl = false # Noncompliant {{Make sure creating a GCP SQL instance without requiring TLS is safe here.}}
+      require_ssl = false # Noncompliant {{Change to disallow unencrypted connections.}}
     # ^^^^^^^^^^^^^^^^^^^
     }
   }
@@ -31,6 +31,25 @@ resource "google_sql_database_instance" "compliant" {
   settings {
     ip_configuration {
       require_ssl = true
+    }
+  }
+}
+
+resource "google_sql_database_instance" "noncompliant_ssl_mode_override" {
+  settings {
+    ip_configuration {
+      ssl_mode    = "ALLOW_UNENCRYPTED_AND_ENCRYPTED" # Noncompliant {{Change to disallow unencrypted connections.}}
+    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+      require_ssl = true
+    }
+  }
+}
+
+resource "google_sql_database_instance" "compliant_ssl_mode" {
+  settings {
+    ip_configuration {
+      ssl_mode    = "ENCRYPTED_ONLY"
+      require_ssl = false
     }
   }
 }
